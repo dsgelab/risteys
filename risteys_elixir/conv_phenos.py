4f499c9e8023ed0e823d5d43eaddf1c8515e6715
Vincent <vincent-octo@users.noreply.github.com>
2019-02-22 07:37:49 +0000

    elixir: change structure of myphenos.json

--- risteys_elixir/conv_phenos.py
+++ risteys_elixir/conv_phenos.py
@@ -6,12 +6,15 @@ with open('assets/data/phenos.json') as f:
     content = json.load(f)
 
 for pheno in content:
+    icds = pheno.get('icd_incl')
+    if icds is not None:
+        icds = list(map(lambda icd: icd.split(":", maxsplit=1), icds))
     res[pheno['phenocode']] = {
         'category': pheno['category'],
         'description': pheno['phenostring'],
         'num_cases': pheno['num_cases'],
         'num_controls': pheno['num_controls'],
-        'icd_incl': pheno.get('icd_incl'),
+        'icd_incl': icds,
         'icd_excl': pheno.get('icd_excl'),
     }
 
