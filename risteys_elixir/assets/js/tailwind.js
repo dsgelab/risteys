5ebbe1942d40d3778473287247383d9643d865ec
Vincent <vincent-octo@users.noreply.github.com>
2019-03-06 15:16:34 +0000

    v1: define a real grey in tailwind CSS
    Reflects on the home page and description page.

--- risteys_elixir/assets/js/tailwind.js
+++ risteys_elixir/assets/js/tailwind.js
@@ -53,7 +53,7 @@ let colors = {
   'grey': '#b8c2cc',
   'grey-light': '#dae1e7',
   'grey-lighter': '#f1f5f8',
-  'grey-lightest': '#f8fafc',
+  'grey-lightest': '#fafafa',
   'white': '#ffffff',
 
   'red-darkest': '#3b0d0c',
