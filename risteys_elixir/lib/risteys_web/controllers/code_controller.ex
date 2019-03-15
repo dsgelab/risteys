b0eaf7aa18df0608332e8b4a85da51420b48b20b
Vincent <vincent-octo@users.noreply.github.com>
2019-03-15 12:05:55 +0000

    v1: remove unused code for key figures

--- risteys_elixir/lib/risteys_web/controllers/code_controller.ex
+++ risteys_elixir/lib/risteys_web/controllers/code_controller.ex
@@ -10,35 +10,11 @@ defmodule RisteysWeb.CodeController do
       |> Jason.decode!()
       |> Map.fetch!(code)
 
-    individuals = Risteys.Data.fake_db(code)
-
-    key_figures = %{
-      all: aggregate(individuals, fn _sex -> true end),
-      male: aggregate(individuals, fn sex -> sex == 1 end),
-      female: aggregate(individuals, fn sex -> sex == 2 end)
-    }
-
     conn
     |> assign(:code, code)
     |> assign(:category, Map.fetch!(pheno, "category"))
     |> assign(:title, Map.fetch!(pheno, "description"))
     |> assign(:icd_incl, Map.fetch!(pheno, "icd_incl"))
-    |> assign(:key_figures, key_figures)
     |> render("show.html")
-  end
-
-  def aggregate(individuals, sex_filter) do
-    ages =
-      individuals
-      |> Enum.filter(fn %{sex: sex} -> sex_filter.(sex) end)
-      |> Enum.map(fn %{age: age} -> age end)
-
-    nevents = length(ages)
-    mean_age = Enum.sum(ages) / length(ages)
-
-    %{
-      nevents: nevents,
-      mean_age: mean_age
-    }
   end
 end
