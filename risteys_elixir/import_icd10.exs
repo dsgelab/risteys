e19c363ce43866c0b781199ab8f3768f29287a4a
Vincent <vincent-octo@users.noreply.github.com>
2019-04-02 10:43:33 +0300

    v1: use Finn + US ICD10 on import

--- risteys_elixir/import_icd10.exs
+++ risteys_elixir/import_icd10.exs
@@ -2,10 +2,26 @@ alias Risteys.{Repo, ICD10}
 
 Logger.configure(level: :info)
 
-"assets/data/icd10cm_codes_2019.tsv"
-|> File.stream!()
-|> CSV.decode!(separator: ?\t)
-|> Stream.map(fn [code, description] ->
-  %ICD10{code: code, description: description}
+# US ICD-10s
+icd10s =
+  "assets/data/icd10cm_codes_2019.tsv"
+  |> File.stream!()
+  |> CSV.decode!(separator: ?\t)
+  |> Enum.reduce(%{}, fn [code, description], acc ->
+    Map.put(acc, code, description)
+  end)
+
+# Finnish ICD-10s
+icd10s =
+  "assets/data/ICD10_finn_codedesc.tsv"
+  |> File.stream!()
+  |> CSV.decode!(separator: ?\t, headers: true)
+  |> Enum.reduce(icd10s, fn %{"code" => code, "longname" => description}, acc ->
+    code = code |> String.replace(".", "")
+    Map.put_new(acc, code, description)
+  end)
+
+icd10s
+|> Enum.map(fn {code, description} ->
+  Repo.insert!(%ICD10{code: code, description: description})
 end)
-|> Enum.each(&Repo.insert!(&1))
