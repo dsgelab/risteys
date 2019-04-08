3e1f9fd2e1514b9bfae84a573d9eb63a885e7cfe
Vincent Llorens <vincent.llorens@users.noreply.version.helsinki.fi>
2019-04-08 10:41:17 +0000

    update and fix tests
    In the process, a data fixture has been added to "Risteys.DataCase".
    It is often useful to use it for testing to temporarily put some data
    in the database.

--- risteys_elixir/test/risteys_web/channels/key_figures_channel_test.exs
+++ risteys_elixir/test/risteys_web/channels/key_figures_channel_test.exs
@@ -6,26 +6,34 @@ defmodule RisteysWeb.KeyFiguresChannelTest do
       socket(RisteysWeb.UserSocket, "user_id", %{some: :assign})
       |> subscribe_and_join(RisteysWeb.KeyFiguresChannel, "key_figures")
 
+    Risteys.DataCase.data_fixture("XYZ00", 20)
+
     {:ok, socket: socket}
   end
 
   test "push initial data", %{socket: socket} do
-    ref = push(socket, "initial_data", %{"body" => "J11"})
+    ref = push(socket, "initial_data", %{"body" => "XYZ00"})
 
     assert_reply ref, :ok, %{
       body: %{
         results: %{
           all: %{
             nevents: _,
-            mean_age: _
+            mean_age: _,
+            case_fatality: _,
+            rehosp: _
           },
           male: %{
             nevents: _,
-            mean_age: _
+            mean_age: _,
+            case_fatality: _,
+            rehosp: _
           },
           female: %{
             nevents: _,
-            mean_age: _
+            mean_age: _,
+            case_fatality: _,
+            rehosp: _
           }
         }
       }
@@ -35,7 +43,7 @@ defmodule RisteysWeb.KeyFiguresChannelTest do
   test "push new data after receiving a 'filter_out'", %{socket: socket} do
     req_payload = %{
       "body" => %{
-        "path" => "/code/X00",
+        "path" => "/code/XYZ00",
         "filters" => %{
           "age" => [40, 50]
         }
@@ -49,15 +57,21 @@ defmodule RisteysWeb.KeyFiguresChannelTest do
         results: %{
           all: %{
             nevents: _,
-            mean_age: _
-          },
-          female: %{
-            nevents: _,
-            mean_age: _
+            mean_age: _,
+            case_fatality: _,
+            rehosp: _
           },
           male: %{
             nevents: _,
-            mean_age: _
+            mean_age: _,
+            case_fatality: _,
+            rehosp: _
+          },
+          female: %{
+            nevents: _,
+            mean_age: _,
+            case_fatality: _,
+            rehosp: _
           }
         }
       }
--- risteys_elixir/test/support/data_case.ex
+++ risteys_elixir/test/support/data_case.ex
@@ -12,6 +12,10 @@ defmodule Risteys.DataCase do
   of the test unless the test case is marked as async.
   """
 
+  alias Risteys.HealthEvent
+  alias Risteys.Phenocode
+  alias Risteys.Repo
+
   use ExUnit.CaseTemplate
 
   using do
@@ -49,5 +53,72 @@ defmodule Risteys.DataCase do
         String.replace(acc, "%{#{key}}", to_string(value))
       end)
     end)
+  end
+
+  @doc """
+  A helper that puts the minimum required data in database for Risteys to run.
+  """
+  def data_fixture(code, count) do
+    phenocode = %Phenocode{
+      code: code,
+      longname: "",
+      tags: "",
+      level: "",
+      omit: false,
+      sex: 1,
+      include: "",
+      pre_conditions: "",
+      conditions: "",
+      outpat_icd: "",
+      hd_mainonly: false,
+      hd_icd_10: [""],
+      hd_icd_9: [""],
+      hd_icd_8: "",
+      hd_icd_10_excl: "",
+      hd_icd_9_excl: "",
+      hd_icd_8_excl: "",
+      cod_mainonly: false,
+      cod_icd_10: [""],
+      cod_icd_9: [""],
+      cod_icd_8: "",
+      cod_icd_10_excl: "",
+      cod_icd_9_excl: "",
+      cod_icd_8_excl: "",
+      oper_nom: "",
+      oper_hl: "",
+      oper_hp1: "",
+      oper_hp2: "",
+      kela_reimb: "",
+      kela_reimb_icd: [""],
+      kela_atc_needother: "",
+      kela_atc: "",
+      canc_topo: "",
+      canc_morph: "",
+      canc_behav: 0,
+      special: "",
+      version: "",
+      source: "",
+      pheweb: false
+    }
+
+    {:ok, phenocode} = Repo.insert(phenocode)
+
+    for i <- 1..count do
+      sex =
+        case rem(i, 2) do
+          0 -> 1
+          1 -> 2
+        end
+
+      health_event = %HealthEvent{
+        age: 50.0,
+        dateevent: ~D[2000-01-01],
+        eid: i,
+        sex: sex
+      }
+
+      assoc = Ecto.build_assoc(phenocode, :health_events, health_event)
+      {:ok, _} = Repo.insert(assoc)
+    end
   end
 end
