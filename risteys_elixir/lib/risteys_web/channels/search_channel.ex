331df1e72f5906f36afe191febe9ea5f5375b239
Vincent <vincent-octo@users.noreply.github.com>
2019-05-07 12:54:45 +0000

    v1/db: improve search by ICD and phenocode
    github: #3

--- risteys_elixir/lib/risteys_web/channels/search_channel.ex
+++ risteys_elixir/lib/risteys_web/channels/search_channel.ex
@@ -1,6 +1,6 @@
 defmodule RisteysWeb.SearchChannel do
   use Phoenix.Channel
-  alias Risteys.{Repo, Phenocode}
+  alias Risteys.{Repo, Phenocode, Icd10, PhenocodeIcd10}
   alias RisteysWeb.Router.Helpers, as: Routes
   import Ecto.Query
 
@@ -22,48 +22,28 @@ defmodule RisteysWeb.SearchChannel do
     {:noreply, socket}
   end
 
-  defp search_icd_code(user_query, limit) do
-    Repo.all(
-      from p in Phenocode,
-        where:
-          ^user_query in p.hd_icd_10 or
-            ^user_query in p.hd_icd_9 or
-            ^user_query in p.cod_icd_10 or
-            ^user_query in p.cod_icd_9 or
-            ^user_query in p.kela_reimb_icd,
-        limit: ^limit
-    )
-    |> struct_icd_code()
-  end
-
-  defp struct_icd_code(phenocodes) do
-    Enum.map(phenocodes, fn %Phenocode{
-                              code: code,
-                              hd_icd_10: hd_icd_10,
-                              hd_icd_9: hd_icd_9,
-                              cod_icd_10: cod_icd_10,
-                              cod_icd_9: cod_icd_9,
-                              kela_reimb_icd: kela_reimb_icd
-                            } ->
-      icds = hd_icd_10 ++ hd_icd_9 ++ cod_icd_10 ++ cod_icd_9 ++ kela_reimb_icd
-      icds = MapSet.new(icds)
-
-      %{
-        icds: icds,
-        code: code
-      }
-    end)
-  end
-
-  defp search_phenocode_code(user_query, limit) do
+  defp search_icd10_code(user_query, limit) do
     pattern = "%" <> user_query <> "%"
 
-    Repo.all(
-      from p in Phenocode,
-        where: ilike(p.code, ^pattern),
-        select: %{code: p.code, name: p.longname},
-        limit: ^limit
-    )
+    query = from p in Phenocode,
+      join: assoc in PhenocodeIcd10,
+      on: p.id == assoc.phenocode_id,
+      join: icd in Icd10,
+      on: assoc.icd10_id == icd.id,
+      where: ilike(icd.code, ^pattern),
+      group_by: p.name,
+      select: %{name: p.name, icds: fragment("array_agg(?)", icd.code)},
+      limit: ^limit
+
+    Repo.all(query)
+    |> Enum.map(&struct_icd_code(&1))
+  end
+
+  defp struct_icd_code(%{name: name, icds: icds}) do
+    %{
+      name: name,
+      icds: MapSet.new(icds) |> MapSet.to_list()  # dedup ICDs
+    }
   end
 
   defp search_phenocode_name(user_query, limit) do
@@ -71,45 +51,56 @@ defmodule RisteysWeb.SearchChannel do
 
     Repo.all(
       from p in Phenocode,
+        where: ilike(p.name, ^pattern),
+        select: %{name: p.name, longname: p.longname},
+        limit: ^limit
+    )
+  end
+
+  defp search_phenocode_longname(user_query, limit) do
+    pattern = "%" <> user_query <> "%"
+
+    Repo.all(
+      from p in Phenocode,
         where: ilike(p.longname, ^pattern),
-        select: %{code: p.code, name: p.longname},
+        select: %{name: p.name, longname: p.longname},
         limit: ^limit
     )
   end
 
   defp search(socket, user_query, limit) do
     # 1. Get matches from the database
-    icds = search_icd_code(user_query, limit)
-    pheno_codes = search_phenocode_code(user_query, limit)
-    pheno_names = search_phenocode_name(user_query, limit)
+    icds = search_icd10_code(user_query, limit)
+    phenocode_names = search_phenocode_name(user_query, limit)
+    phenocode_longnames = search_phenocode_longname(user_query, limit)
 
     # 2. Structure the output to be sent over the channel
     icds = [
       "ICD-10 code",
-      Enum.map(icds, fn %{icds: icds, code: phenocode} ->
+      Enum.map(icds, fn %{icds: icds, name: name} ->
         icds = Enum.join(icds, ", ")
         icds = highlight(icds, user_query)
-        %{phenocode: phenocode, content: icds, url: url(socket, phenocode)}
+        %{phenocode: name, content: icds, url: url(socket, name)}
       end)
     ]
 
-    pheno_codes = [
-      "Phenocode code",
-      Enum.map(pheno_codes, fn %{code: code, name: name} ->
-        hlcode = highlight(code, user_query)
-        %{phenocode: hlcode, content: name, url: url(socket, code)}
-      end)
-    ]
-
-    pheno_names = [
+    phenocode_names = [
       "Phenocode name",
-      Enum.map(pheno_names, fn %{code: code, name: name} ->
-        name = highlight(name, user_query)
-        %{phenocode: code, content: name, url: url(socket, code)}
+      Enum.map(phenocode_names, fn %{name: name, longname: longname} ->
+        hlcode = highlight(name, user_query)
+        %{phenocode: hlcode, content: longname, url: url(socket, name)}
       end)
     ]
 
-    [icds, pheno_codes, pheno_names]
+    phenocode_longnames = [
+      "Phenocode long name",
+      Enum.map(phenocode_longnames, fn %{name: name, longname: longname} ->
+        name = highlight(name, user_query)
+        %{phenocode: name, content: longname, url: url(socket, name)}
+      end)
+    ]
+
+    [icds, phenocode_names, phenocode_longnames]
     |> Enum.reject(fn [_category, list] -> Enum.empty?(list) end)
   end
 
