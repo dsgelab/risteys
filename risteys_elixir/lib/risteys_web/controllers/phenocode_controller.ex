895a3397eebca88e7ed1f0498139df8f3449fc1a
Vincent <vincent-octo@users.noreply.github.com>
2019-05-08 11:44:27 +0000

    v1/db: phenocode page use new DB structure

--- risteys_elixir/lib/risteys_web/controllers/phenocode_controller.ex
+++ risteys_elixir/lib/risteys_web/controllers/phenocode_controller.ex
@@ -1,102 +1,115 @@
 defmodule RisteysWeb.PhenocodeController do
   use RisteysWeb, :controller
-  alias Risteys.{Repo, Icd9, Icd10, Phenocode}
+  alias Risteys.{Repo, Icd9, Icd10, Phenocode, PhenocodeIcd10, PhenocodeIcd9, StatsSex}
   import Ecto.Query
   import Phoenix.HTML
 
   def show(conn, %{"name" => name}) do
     phenocode = Repo.get_by(Phenocode, name: name)
 
-    # TODO(vincent) might be better to have a true "db relationship" between a phenocode
-    # and the ICD-10/9 columns that are arrays.
+    # Get the descriptions, if any.
+    no_desc = ["No definition available."]
 
-    # Get the descriptions for the ICD-10 and ICD-9 linked in this phenocode
-    icd10s =
-      (phenocode.hd_icd_10 ++ phenocode.cod_icd_10 ++ phenocode.kela_reimb_icd)
-      |> MapSet.new()
-
-    icd10s =
-      for icd <- icd10s, into: %{} do
-        description = Repo.one(from i in Icd10, where: i.code == ^icd, select: i.description)
-        {icd, description}
+    {descriptions, ontology} =
+      if not is_nil(phenocode.ontology) do
+        Map.pop(phenocode.ontology, "DESCRIPTION", no_desc)
+      else
+        {no_desc, %{}}
       end
 
-    icd9s =
-      (phenocode.hd_icd_9 ++ phenocode.cod_icd_9)
-      |> MapSet.new()
-
-    icd9s =
-      for icd <- icd9s, into: %{} do
-        description = Repo.one(from i in Icd9, where: i.code == ^icd, select: i.description)
-        {icd, description}
-      end
+    stats = get_stats(phenocode)
 
     conn
     |> assign(:name, phenocode.name)
     |> assign(:title, phenocode.longname)
-    |> assign(:data_sources, data_sources(phenocode, icd10s, icd9s))
-    |> assign(:plots_data, plots_data(phenocode.name))
+    |> assign(:data_sources, data_sources(phenocode))
+    |> assign(:ontology, ontology)
+    |> assign(:stats, stats)
+    |> assign(:distrib_year, phenocode.distrib_year)
+    |> assign(:distrib_age, phenocode.distrib_age)
+    |> assign(:descriptions, descriptions)
     |> render("show.html")
   end
 
-  defp data_sources(phenocode, icd10s, icd9s) do
-    descriptions = [
-      omit: "Do NOT RELEASE this endpoint ",
-      sex: "SEX specific endpoint",
-      conditions: "CONDITIONS required",
-      outpat_icd: "Outpatient visit: ICD and other codes ",
-      oper_nom: "Operations: NOMESCO codes",
-      oper_hl: "Operations: FINNISH HOSPITAL LEAGUE codes",
-      oper_hp1: "Operations: HEART PATIENT codes V1",
-      oper_hp2: "Operations: HEART PATIENT codes V2",
-      kela_reimb:
-        ~E"Reimbursements: <abbr data-title=\"Finnish Social Insurance Institution\">KELA</abbr> codes",
-      kela_reimb_icd: "Reimbursements: ICD codes",
-      kela_atc_needother: "Medicine purchases: ATC; other reg. data required",
-      kela_atc: "Medicine purchases: ATC codes",
-      canc_topo: "Cancer reg: TOPOGRAPHY codes",
-      canc_morph: "Cancer reg: MORPHOLOGY codes"
-    ]
+  defp data_sources(phenocode) do
+    icd10s =
+      Repo.all(
+        from assoc in PhenocodeIcd10,
+          join: p in Phenocode,
+          on: assoc.phenocode_id == p.id,
+          join: icd in Icd10,
+          on: assoc.icd10_id == icd.id,
+          where: p.id == ^phenocode.id,
+          select: %{registry: assoc.registry, icd: icd}
+      )
 
-    hd_icd_10 =
-      phenocode.hd_icd_10
-      |> Enum.map(&abbr_icd(&1, icd10s))
+    hd_icd10s = filter_icds_registry(icd10s, "HD")
+    cod_icd10s = filter_icds_registry(icd10s, "COD")
+    kela_icd10s = filter_icds_registry(icd10s, "KELA_REIMB")
 
-    hd_icd_9 =
-      phenocode.hd_icd_9
-      |> Enum.map(&abbr_icd(&1, icd9s))
+    icd9s =
+      Repo.all(
+        from assoc in PhenocodeIcd9,
+          join: p in Phenocode,
+          on: assoc.phenocode_id == p.id,
+          join: icd in Icd9,
+          on: assoc.icd9_id == icd.id,
+          where: p.id == ^phenocode.id,
+          select: %{registry: assoc.registry, icd: icd}
+      )
 
-    hd_codes = hd_icd_10 ++ hd_icd_9 ++ [phenocode.hd_icd_8]
+    hd_icd9s = filter_icds_registry(icd9s, "HD")
+    cod_icd9s = filter_icds_registry(icd9s, "COD")
 
-    cod_icd_10 =
-      phenocode.cod_icd_10
-      |> Enum.map(&abbr_icd(&1, icd10s))
+    %{
+      hd_icd10s: hd_icd10s,
+      cod_icd10s: cod_icd10s,
+      kela_icd10s: kela_icd10s,
+      hd_icd9s: hd_icd9s,
+      cod_icd9s: cod_icd9s,
+      hd_icd8s: phenocode.hd_icd_8,
+      cod_icd8s: phenocode.cod_icd_8,
+      outpat_icd: phenocode.outpat_icd,
+      oper_nom: phenocode.oper_nom,
+      oper_hl: phenocode.oper_hl,
+      oper_hp1: phenocode.oper_hp1,
+      oper_hp2: phenocode.oper_hp2,
+      kela_reimb: phenocode.kela_reimb,
+      kela_atc_needother: phenocode.kela_atc_needother,
+      kela_atc: phenocode.kela_atc,
+      canc_topo: phenocode.canc_topo,
+      canc_morph: phenocode.canc_morph,
+      omit: phenocode.omit,
+      sex: phenocode.sex,
+      conditions: phenocode.conditions
+    }
+  end
 
-    cod_icd_9 =
-      phenocode.cod_icd_9
-      |> Enum.map(&abbr_icd(&1, icd9s))
-
-    cod_codes = cod_icd_10 ++ cod_icd_9 ++ [phenocode.cod_icd_8]
-
-    data_table =
-      descriptions
-      |> Enum.map(fn {column, description} ->
-        {description, Map.fetch!(phenocode, column)}
-      end)
-
-    (data_table ++
-       [{"Hospital Discharge registry", hd_codes}, {"Cause of death registry", cod_codes}])
-    |> Enum.reject(fn {_description, value} ->
-      value in ["", nil, []]
+  defp filter_icds_registry(icds, wanted_registry) do
+    Enum.reduce(icds, [], fn %{registry: registry, icd: icd}, acc ->
+      if registry == wanted_registry do
+        acc ++ [icd]
+      else
+        acc
+      end
     end)
   end
 
-  defp abbr_icd(code, icds) do
-    desc = Map.fetch!(icds, code)
-    ~E"<abbr data-title=\"<%= desc %>\"><%= code %></abbr>"
+  defp get_stats(phenocode) do
+    stats = Repo.all(from ss in StatsSex, where: ss.phenocode_id == ^phenocode.id)
+
+    [stats_all] = Enum.filter(stats, fn stats_sex -> stats_sex.sex == 0 end)
+    [stats_female] = Enum.filter(stats, fn stats_sex -> stats_sex.sex == 2 end)
+    [stats_male] = Enum.filter(stats, fn stats_sex -> stats_sex.sex == 1 end)
+
+    %{
+      all: stats_all,
+      female: stats_female,
+      male: stats_male
+    }
   end
 
-  def plots_data(phenocode_name) do
+  defp plots_data(phenocode_name) do
     bin_by_age = Risteys.Data.bin_by_age(phenocode_name)
 
     bin_by_age =
--- risteys_elixir/lib/risteys_web/views/phenocode_view.ex
+++ risteys_elixir/lib/risteys_web/views/phenocode_view.ex
@@ -1,4 +1,89 @@
 defmodule RisteysWeb.PhenocodeView do
   use RisteysWeb, :view
 
+  defp sorted_descriptions(descriptions) do
+    Enum.sort(descriptions, fn a, b -> String.length(a) > String.length(b) end)
+  end
+
+  defp table_data_sources(data_sources) do
+    # Merge HD registry ICDs
+    hd_icd10s = render_icds("ICD-10: ", data_sources.hd_icd10s)
+    hd_icd9s = render_icds("ICD-9: ", data_sources.hd_icd9s)
+
+    hd_icd8s =
+      if not is_nil(data_sources.hd_icd8s) do
+        "ICD-8: " <> data_sources.hd_icd8s
+      else
+        ""
+      end
+
+    hd = [hd_icd10s, hd_icd9s, hd_icd8s]
+    hd = Enum.reject(hd, fn val -> val == "" end)
+    hd = Enum.intersperse(hd, ", ")
+
+    # Merge COD registry ICDs
+    cod_icd10s = render_icds("ICD-10: ", data_sources.cod_icd10s)
+    cod_icd9s = render_icds("ICD-9: ", data_sources.cod_icd9s)
+
+    cod_icd8s =
+      if not is_nil(data_sources.cod_icd8s) do
+        "ICD-8: " <> data_sources.cod_icd8s
+      else
+        ""
+      end
+
+    cod = [cod_icd10s, cod_icd9s, cod_icd8s]
+    cod = Enum.reject(cod, fn val -> val == "" end)
+    cod = Enum.intersperse(cod, ", ")
+
+    kela_icd10s = render_icds("ICD-10: ", data_sources.kela_icd10s)
+
+    # Build the whole table
+    kela_abbr = abbr("KELA", "Finnish Social Insurance Institution")
+
+    table = [
+      {"Hospital Discharge registry", hd},
+      {"Cause of Death registry", cod},
+      {[kela_abbr | " reimbursements"], kela_icd10s},
+      {"Outpatient visit: ICD and other codes ", data_sources.outpat_icd},
+      {"Operations: NOMESCO codes", data_sources.oper_nom},
+      {"Operations: FINNISH HOSPITAL LEAGUE codes", data_sources.oper_hl},
+      {"Operations: HEART PATIENT codes V1", data_sources.oper_hp1},
+      {"Operations: HEART PATIENT codes V2", data_sources.oper_hp2},
+      {[kela_abbr | " reimboursements codes"], data_sources.kela_reimb},
+      {"Medicine purchases: ATC; other reg. data required", data_sources.kela_atc_needother},
+      {"Medicine purchases: ATC codes", data_sources.kela_atc},
+      {"Cancer reg: TOPOGRAPHY codes", data_sources.canc_topo},
+      {"Cancer reg: MORPHOLOGY codes", data_sources.canc_morph},
+      {"Do NOT RELEASE this endpoint ", data_sources.omit},
+      {"SEX specific endpoint", data_sources.sex},
+      {"CONDITIONS required", data_sources.conditions}
+    ]
+
+    # Discard table rows with no values
+    Enum.reject(table, fn {_name, values} -> values in ["", nil, ["", "", ""]] end)
+  end
+
+  defp render_icds(_prefix, nil), do: ""
+  defp render_icds(_prefix, []), do: ""
+
+  defp render_icds(prefix, icds) do
+    icds =
+      icds
+      |> Enum.map(fn icd -> abbr(icd.code, icd.description) end)
+      |> Enum.intersperse("/")
+
+    [prefix | icds]
+  end
+
+  defp table_ontology(ontology) do
+    for {source, values} <- ontology, into: %{} do
+      values = Enum.intersperse(values, ", ")
+      {source, values}
+    end
+  end
+
+  defp abbr(text, title) do
+    content_tag(:abbr, text, [{:data, [title: title]}])
+  end
 end
