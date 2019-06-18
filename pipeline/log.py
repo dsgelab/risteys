7afb0bc0a0c1a162515f7f5796e4b636ad82d299
Vincent <vincent-octo@users.noreply.github.com>
2019-06-18 11:48:06 +0000

    pipeline: logger load level from env var

--- pipeline/log.py
+++ pipeline/log.py
@@ -1,14 +1,15 @@
 import logging
+from os import getenv
 
+
+level = getenv("LOG_LEVEL", logging.INFO)
 logger = logging.getLogger("pipeline")
-
 handler = logging.StreamHandler()
-
 formatter = logging.Formatter(
     "%(asctime)s %(levelname)-5s %(module)-21s %(funcName)-25s: %(message)s",
     "%H:%M:%S"
 )
-handler.setFormatter(formatter)
 
+handler.setFormatter(formatter)
 logger.addHandler(handler)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(level)
