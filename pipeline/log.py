626c20c64933d58a6d545861018e5e5f8fc6e113
Vincent <vincent-octo@users.noreply.github.com>
2019-08-05 06:11:27 +0000

    pipeline: display date in log output

--- pipeline/log.py
+++ pipeline/log.py
@@ -6,9 +6,7 @@ level = getenv("LOG_LEVEL", logging.INFO)
 logger = logging.getLogger("pipeline")
 handler = logging.StreamHandler()
 formatter = logging.Formatter(
-    "%(asctime)s %(levelname)-5s %(module)-21s %(funcName)-25s: %(message)s",
-    "%H:%M:%S"
-)
+    "%(asctime)s %(levelname)-8s %(module)-21s %(funcName)-25s: %(message)s")
 
 handler.setFormatter(formatter)
 logger.addHandler(handler)
