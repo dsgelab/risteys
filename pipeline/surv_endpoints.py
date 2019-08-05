ca5694e5d6d18a5f4fb624775d2aa089bd570c35
Vincent <vincent-octo@users.noreply.github.com>
2019-08-05 09:23:47 +0000

    pipeline: filtering pairs on all endpoints
    Do not restrict pairs filtering to #DEMO endpoints, do it for all
    endpoints.

--- pipeline/surv_endpoints.py
+++ pipeline/surv_endpoints.py
@@ -9,8 +9,6 @@ Input files
 -----------
 - input.hdf5
   Each row is an event with FinnGen ID, Endpoint and time information.
-- demo_endpoints.tsv
-  List of #DEMO endpoints, one endpoint per line
 
 Description
 -----------
@@ -24,11 +22,9 @@ contains E. That is:
 
 This is a lot endpoint combinations.
 
-We filter the possible endpoint combinations:
-- by only selecting endpoints that have enough individuals in order to
-  have enough statistical power
-- by selecting only #DEMO endpoints as endpoints of interest (so every
-  pair containing any endpoint and 1 endpoint of interest)
+We filter the possible endpoint combinations by only selecting
+endpoints that have enough individuals in order to have enough
+statistical power
 """
 
 from pathlib import Path
@@ -43,7 +39,6 @@ from log import logger
 # Files
 DATA_DIR = Path(argv[1])
 INPUT_EVENTS = "input.hdf5"
-INPUT_DEMO_ENDPOINTS = "demo_endpoints.tsv"
 OUTPUT_NAME = "filtered_pairs.csv"
 
 # Filter parameters
@@ -53,11 +48,10 @@ LATER_THRESHOLD = 25 # how many individuals to have, at least, for the
                      # "later" endpoints
 
 
-def prechecks(events_path, demo_endpoints_path, output_path):
+def prechecks(events_path, output_path):
     """Perform checks before running to fail earlier rather than later"""
     logger.info("Performing pre-checks")
     assert events_path.exists(), f"{events_path} doesn't exist"
-    assert demo_endpoints_path.exists(), f"{demo_endpoints_path} doesn't exist"
     assert not output_path.exists(), f"{output_path} already exists, not overwriting it"
 
     # Check event file headers
@@ -67,31 +61,28 @@ def prechecks(events_path, demo_endpoints_path, output_path):
     assert expected_cols.issubset(cols), f"wrong columns in input file: {expected_cols} not in {cols}"
 
 
-def main(events_path, demo_endpoints_path, output_path):
+def main(events_path, output_path):
     """Get a selection of pairs of endpoints to do survival analysis on"""
-    prechecks(events_path, demo_endpoints_path, output_path)
+    prechecks(events_path, output_path)
 
-    (df, demo_endpoints) = load_data(events_path, demo_endpoints_path)
+    df = load_data(events_path)
     matrix = build_matrix(df)
-    pairs = build_pairs(matrix, demo_endpoints)
+    pairs = build_pairs(matrix)
     pairs = filter_prior_later(pairs, matrix)
     pairs = filter_crosstab(pairs, matrix)
     write_output(pairs, output_path)
 
 
-def load_data(events_path, demo_endpoints_path):
+def load_data(events_path):
     """Load input data"""
     logger.info("Loading data")
-    # Get the list of #DEMO endpoints
-    demo = pd.read_csv(demo_endpoints_path).values.flatten()
-
     # Read the longitudinal file
     df = pd.read_hdf(events_path)
 
     # Keep only events after 1998, when we have registry data
     df = df[df.EVENT_YEAR.gt(1997)]
 
-    return (df, demo)
+    return df
 
 
 def build_matrix(df):
@@ -108,15 +99,12 @@ def build_matrix(df):
     return matrix
 
 
-def build_pairs(matrix, demo_endpoints):
-    """Build a list of pairs of endpoints, with #DEMO endpoints as endpoints of interest"""
+def build_pairs(matrix):
+    """Build a list of pairs of endpoints"""
     logger.info("Building full list of pairs")
     endpoint_counts = matrix.count()  # count n. individuals by endpoints
     prior_endpoints = endpoint_counts[endpoint_counts >= CROSS_THRESHOLD].index
-    later_endpoints = endpoint_counts[
-        (endpoint_counts >= LATER_THRESHOLD)
-        & (endpoint_counts.index.isin(demo_endpoints))
-    ].index
+    later_endpoints = endpoint_counts[endpoint_counts >= LATER_THRESHOLD].index
 
     # Build all possible pairs of endpoints, filter them out after
     pairs = []
@@ -192,6 +180,5 @@ def write_output(pairs, output_path):
 if __name__ == '__main__':
     main(
         DATA_DIR / INPUT_EVENTS,
-        DATA_DIR / INPUT_DEMO_ENDPOINTS,
         DATA_DIR / OUTPUT_NAME,
     )
