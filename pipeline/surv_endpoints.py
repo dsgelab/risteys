90e6c2ae2a7690465bea6f06826cc553e40609c2
Vincent <vincent-octo@users.noreply.github.com>
2020-02-21 11:26:44 +0000

    pipeline: add medication stats script
    This is the pipeline part of adding medication statistics to Risteys,
    web part to follow.
    
    A sensitivity analysis was done to determine what and model and
    parameters to use. This commit only contains the result (logit model
    and fixed parameters in the script).
    
    GitHub: references #21

--- pipeline/surv_endpoints.py
+++ pipeline/surv_endpoints.py
@@ -45,6 +45,7 @@ import re
 import numpy as np
 import pandas as pd
 
+from common import get_all_inclusions
 from log import logger
 
 
@@ -305,43 +306,6 @@ def filter_descendants(pairs, endpoints):
             res.append((prior, later))
 
     return res
-
-
-def get_all_inclusions(endpoints):
-    """Flatten the endpoint hierarchy with a map: endpoint -> all its descendants"""
-    composite = endpoints[endpoints.INCLUDE.notna()]
-    parents = composite.NAME
-    children = composite.INCLUDE.str.split("|")
-
-    # Build the graph of parent -> direct children
-    graph = {}
-    for p, c in zip(parents, children):
-        graph[p] = set(c)
-
-    # Build the map of parent -> all direct and indirect descendants
-    res = {}
-    cyclic = []
-    for ee in endpoints.NAME:
-        res[ee] = get_descendants(ee, graph, cyclic)
-
-    return res
-
-
-def get_descendants(name, graph, cyclic, acc=None):
-    """Get all direct and indirect descendants of an endpoint"""
-    # Initialize for first non tail-call
-    if acc is None:
-        acc = set()
-
-    if name in cyclic:
-        return graph.get(name, set())
-
-    children = graph.get(name, set())
-    children = children.difference(set([name]))  # case where an endpoint includes itself
-    acc = acc.union(children)
-    for child in children:
-        acc = acc.union(get_descendants(child, graph, acc))
-    return acc
 
 
 def filter_prior_later(pairs, matrix):
