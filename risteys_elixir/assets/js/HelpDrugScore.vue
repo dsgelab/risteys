e10a7c0787b2be2cf45096006d519f47fe6bc844
Vincent <vincent-octo@users.noreply.github.com>
2020-04-27 11:55:33 +0000

    risteys: update Methods page
    Add more details on associations.
    Add section on drug statistics.

--- risteys_elixir/assets/js/HelpDrugScore.vue
+++ risteys_elixir/assets/js/HelpDrugScore.vue
@@ -2,6 +2,7 @@
 	<HelpButton>
 		<h4>Drug Score</h4>
 		<p>Probability of getting the drug after <i>{{ phenocode }}</i> conditional on having <i>{{ phenocode }}</i>.</p>
+		<p>See <i>Drug Statistics</i> on the <a href="/methods#drug-stats">Methods page</a> for more information.</p>
 	</HelpButton>
 </template>
 
