af563aafa4ba67b8bbae55f4c41159d47135445a
Vincent <vincent-octo@users.noreply.github.com>
2020-11-18 13:07:14 +0000

    risteys: add BCH stats (+ db migration)
    Add the baseline cumulative hazard stats, along side other variables
    extracted from the fitted Cox model. This will allow to compute the
    absolute risk on the fly.

--- risteys_elixir/import_coxhr.exs
+++ risteys_elixir/import_coxhr.exs
@@ -6,17 +6,19 @@
 # where <csv-file-cox> is a CSV file with data for many pairs of
 # endpoints, with the following header columns:
 # - prior
-# - later
-# - lagged_hr_cut_year
-# - nindivs_prior_later
-# - median_duration
-# - pred_coef
-# - pred_se
-# - pred_hr
-# - pred_ci_lower
-# - pred_ci_upper
-# - pred_pval
-# - pred_zval
+# - outcome
+# - lag_hr
+# - step_size
+# - nindivs_prior_outcome
+# - absolute_risk
+# - prior_coef
+# - prior_se
+# - prior_hr
+# - prior_ci_lower
+# - prior_ci_upper
+# - prior_pval
+# - prior_zval
+# - prior_norm_mean
 # - year_coef
 # - year_se
 # - year_hr
@@ -24,6 +26,7 @@
 # - year_ci_upper
 # - year_pval
 # - year_zval
+# - year_norm_mean
 # - sex_coef
 # - sex_se
 # - sex_hr
@@ -31,23 +34,18 @@
 # - sex_ci_upper
 # - sex_pval
 # - sex_zval
-# - nsubjects
-# - nevents
-# - partial_log_likelihood
-# - concordance
-# - log_likelihood_ratio_test
-# - log_likelihood_ndf
-# - log_likelihood_pval
-#
-# NOTE For now only the following information is imported:
-# - prior
-# - later
-# - lagged_hr_cut_year
-# - pred_hr
-# - pred_ci_lower
-# - pred_ci_upper
-# - pred_pval
-# - nindivs_prior_later
+# - sex_norm_mean
+# - bch
+# - bch_0
+# - bch_2.5
+# - bch_5
+# - bch_7.5
+# - bch_10
+# - bch_12.5
+# - bch_15
+# - bch_17.5
+# - bch_20
+# - bch_21.99
 
 alias Risteys.{Repo, CoxHR, Phenocode}
 import Ecto.Query
@@ -63,11 +61,13 @@ coxhr_filepath
 |> CSV.decode!(headers: true)
 |> Stream.filter(fn %{"prior" => prior, "outcome" => outcome} ->
   prior_in_phenos = MapSet.member?(phenos, prior)
+
   if not prior_in_phenos do
     Logger.warn("Prior #{prior} not found in phenos")
   end
 
   outcome_in_phenos = MapSet.member?(phenos, outcome)
+
   if not outcome_in_phenos do
     Logger.warn("Outcome #{outcome} not found in phenos")
   end
@@ -83,7 +83,23 @@ end)
                    "prior_ci_lower" => ci_min,
                    "prior_ci_upper" => ci_max,
                    "prior_pval" => pvalue,
-                   "nindivs_prior_outcome" => n_individuals
+                   "nindivs_prior_outcome" => n_individuals,
+                   "prior_coef" => prior_coef,
+                   "prior_norm_mean" => prior_norm_mean,
+                   "year_coef" => year_coef,
+                   "year_norm_mean" => year_norm_mean,
+                   "sex_coef" => sex_coef,
+                   "sex_norm_mean" => sex_norm_mean,
+                   "bch_0" => bch_year_0,
+                   "bch_2.5" => bch_year_2p5,
+                   "bch_5" => bch_year_5,
+                   "bch_7.5" => bch_year_7p5,
+                   "bch_10" => bch_year_10,
+                   "bch_12.5" => bch_year_12p5,
+                   "bch_15" => bch_year_15,
+                   "bch_17.5" => bch_year_17p5,
+                   "bch_20" => bch_year_20,
+                   "bch_21.99" => bch_year_21p99
                  }, idx} ->
   Logger.debug("Processing pair: #{prior} -> #{outcome}")
 
@@ -98,6 +114,9 @@ end)
     else
       lagged_years |> String.to_integer() |> trunc
     end
+
+  sex_coef = if sex_coef == "nan", do: nil, else: String.to_float(sex_coef)
+  sex_norm_mean = if sex_norm_mean == "nan", do: nil, else: String.to_float(sex_norm_mean)
 
   case hr do
     "nan" ->
@@ -125,7 +144,23 @@ end)
           ci_min: String.to_float(ci_min),
           ci_max: String.to_float(ci_max),
           pvalue: String.to_float(pvalue),
-          n_individuals: String.to_integer(n_individuals)
+          n_individuals: String.to_integer(n_individuals),
+          prior_coef: String.to_float(prior_coef),
+          prior_norm_mean: String.to_float(prior_norm_mean),
+          year_coef: String.to_float(year_coef),
+          year_norm_mean: String.to_float(year_norm_mean),
+          sex_coef: sex_coef,
+          sex_norm_mean: sex_norm_mean,
+          bch_year_0: String.to_float(bch_year_0),
+          bch_year_2p5: String.to_float(bch_year_2p5),
+          bch_year_5: String.to_float(bch_year_5),
+          bch_year_7p5: String.to_float(bch_year_7p5),
+          bch_year_10: String.to_float(bch_year_10),
+          bch_year_12p5: String.to_float(bch_year_12p5),
+          bch_year_15: String.to_float(bch_year_15),
+          bch_year_17p5: String.to_float(bch_year_17p5),
+          bch_year_20: String.to_float(bch_year_20),
+          bch_year_21p99: String.to_float(bch_year_21p99)
         })
         |> Repo.insert_or_update()
 
--- risteys_elixir/lib/risteys/cox_hr.ex
+++ risteys_elixir/lib/risteys/cox_hr.ex
@@ -13,13 +13,55 @@ defmodule Risteys.CoxHR do
     field :n_individuals, :integer
     field :pvalue, :float
 
+    field :prior_coef, :float
+    field :year_coef, :float
+    field :sex_coef, :float
+    field :prior_norm_mean, :float
+    field :year_norm_mean, :float
+    field :sex_norm_mean, :float
+    field :bch_year_0, :float
+    field :bch_year_2p5, :float
+    field :bch_year_5, :float
+    field :bch_year_7p5, :float
+    field :bch_year_10, :float
+    field :bch_year_12p5, :float
+    field :bch_year_15, :float
+    field :bch_year_17p5, :float
+    field :bch_year_20, :float
+    field :bch_year_21p99, :float
+
     timestamps()
   end
 
   @doc false
   def changeset(cox_hr, attrs) do
     cox_hr
-    |> cast(attrs, [:prior_id, :outcome_id, :lagged_hr_cut_year, :hr, :ci_min, :ci_max, :n_individuals, :pvalue])
+    |> cast(attrs, [
+	  :prior_id,
+	  :outcome_id,
+	  :lagged_hr_cut_year,
+	  :hr,
+	  :ci_min,
+	  :ci_max,
+	  :n_individuals,
+	  :pvalue,
+	  :prior_coef,
+	  :year_coef,
+	  :sex_coef,
+	  :prior_norm_mean,
+	  :year_norm_mean,
+	  :sex_norm_mean,
+	  :bch_year_0,
+	  :bch_year_2p5,
+	  :bch_year_5,
+	  :bch_year_7p5,
+	  :bch_year_10,
+	  :bch_year_12p5,
+	  :bch_year_15,
+	  :bch_year_17p5,
+	  :bch_year_20,
+	  :bch_year_21p99
+	])
     |> validate_inclusion(:lagged_hr_cut_year, [0, 1, 5, 15])
     |> validate_number(:n_individuals, greater_than: 5)
     |> validate_number(:pvalue, less_than: 1)
