fdd73e24d063dd53ccc74fb2a31825916e62238c
Vincent <vincent-octo@users.noreply.github.com>
2021-01-19 10:54:08 +0000

    maint: reorganize ICD-10 test file

--- risteys_elixir/test/risteys/icd10_test.exs
+++ risteys_elixir/test/risteys/icd10_test.exs
@@ -30,71 +30,28 @@ defmodule Risteys.Icd10Test do
     assert MapSet.equal?(MapSet.new(parsed), MapSet.new(expected))
   end
 
-  describe "HD ICD 10" do
+  describe "Group matching" do
     test "VIII_EAR_MASTOID: H[6-9]", context do
-      # Matches all of ICD chapter VIII: Diseases of the ear and mastoid process
-      # Find highest in hierarchy?
       cell = "H[6-9]"
       expected = ["H60-H95"]
-
       assert_parsed(cell, expected, context)
     end
 
     test "ST19_INJURY_POISO_CERTA_OTHER_CONSE_EXTER_CAUSES: S|T", context do
       cell = "S|T"
       expected = ["S00-T98"]
-
-      assert_parsed(cell, expected, context)
-    end
-
-    test "ENTEROPATH_E_COLI: A040", context do
-      # Somehow the endpoint def don't use the '.' found in the ICD-10
-      cell = "A040"
-      expected = ["A04.0"]
-
-      assert_parsed(cell, expected, context)
-    end
-
-    test "BACT_INTEST_INFECTION_NOS: A04[8-9]", context do
-      cell = "A04[8-9]"
-
-      expected = [
-        "A04.8",
-        "A04.9"
-      ]
-
       assert_parsed(cell, expected, context)
     end
 
     test "AB1_OTHER_BACTERIAL: A[3-4][0-9]", context do
       cell = "A[3-4][0-9]"
       expected = ["A30-A49"]
-
       assert_parsed(cell, expected, context)
     end
 
     test "AB1_SEXUAL_TRANSMISSION: A5[0-9]|A6[0-4]", context do
-      # Handle the "|"
-      # Match for category seems to be found in ICD10 FINN only
       cell = "A5[0-9]|A6[0-4]"
       expected = ["A50-A64"]
-
-      assert_parsed(cell, expected, context)
-    end
-
-    test "P16_MECONIUMI_CAUSED_CYSTIC_FIBROSIS: P75*", context do
-      # Here '*' is not part of a regex, but marking the code as a symptom
-      cell = "P75*"
-      expected = ["P75*"]
-
-      assert_parsed(cell, expected, context)
-    end
-
-    test "P16_MECONIUM_ILEUS1: P75*E841", context do
-      # Here '*' is still not a regex, but the code is the form <symptom> * <reason>
-      cell = "P75*E841"
-      expected = ["P75*E84.1"]
-
       assert_parsed(cell, expected, context)
     end
 
@@ -116,6 +73,105 @@ defmodule Risteys.Icd10Test do
         # doesn't contain I14, like the cell
         "I10-I15",
         "I67.4"
+      ]
+
+      assert_parsed(cell, expected, context)
+    end
+
+    test "I_INFECT_PARASIT: A|B", context do
+      cell = "A|B"
+      expected = ["A00-B99"]
+      assert_parsed(cell, expected, context)
+    end
+
+    test "VI_NERVOUS: G", context do
+      cell = "G"
+      expected = ["G00-G99"]
+      assert_parsed(cell, expected, context)
+    end
+
+    test "XIII_MUSCULOSKELET: M", context do
+      cell = "M"
+      expected = ["M00-M99"]
+      assert_parsed(cell, expected, context)
+    end
+  end
+
+  describe "Symptom / Cause pairs" do
+    test "P16_MECONIUMI_CAUSED_CYSTIC_FIBROSIS: P75*", context do
+      # Here '*' is not part of a regex, but marking the code as a symptom
+      cell = "P75*"
+      expected = ["P75*"]
+      assert_parsed(cell, expected, context)
+    end
+
+    test "P16_MECONIUM_ILEUS1: P75*E841", context do
+      # Here '*' is still not a regex, but the code is the form <symptom> * <reason>
+      cell = "P75*E841"
+      expected = ["P75*E84.1"]
+
+      assert_parsed(cell, expected, context)
+    end
+
+    test "& ➝ +: T58&F0289", context do
+      # From ST19_DEMEN_RELATED_TOXIC_EFFECT_CARBON_MONOX
+      # Interpret "&" as a symptom pair, so either "+" or "*".
+      cell = "T58&F0289"
+      expected = ["T58+F02.89"]
+      assert_parsed(cell, expected, context)
+    end
+
+    test "& ➝ *: G590&E104|G590&E114", context do
+      # From DM_MONONEURO
+      cell = "G590&E104|G590&E114"
+      expected = ["G59.0*E10.4", "G59.0*E11.4"]
+      assert_parsed(cell, expected, context)
+    end
+
+    test "& ➝ * :  M07&L405|M07[1-3]", context do
+      # From M13_PSORIARTH_ICD10
+      cell = "M07[0-3]&L405|M07[1-3]"
+
+      expected = [
+        "M07.0*L40.5",
+        "M07.1*L40.5",
+        "M07.2*L40.5",
+        "M07.3*L40.5",
+        "M07.1*",
+        "M07.2*",
+        "M07.3*"
+      ]
+
+      assert_parsed(cell, expected, context)
+    end
+
+    test "non-matching: F009&G309", context do
+      # From AD_U
+      #
+      # This is a tricky case. The pair F00.9, G30.9 is not found in
+      # the official Finnish ICD-10 document, but the pair appears
+      # however in the FinnGen data.
+      # Best course of action is to not match anything on this pair so
+      # it is displayed as is in Risteys.
+      cell = "F009&G309"
+      expected = []
+      assert_parsed(cell, expected, context)
+    end
+  end
+
+  describe "Misc." do
+    test "ENTEROPATH_E_COLI: A040", context do
+      cell = "A040"
+      expected = ["A04.0"]
+      assert_parsed(cell, expected, context)
+    end
+
+    test "BACT_INTEST_INFECTION_NOS: A04[8-9]", context do
+      cell = "A04[8-9]"
+
+      expected = [
+        "A04.8",
+        "A04.9"
       ]
 
       assert_parsed(cell, expected, context)
@@ -187,83 +243,24 @@ defmodule Risteys.Icd10Test do
       expected = ["G00", "G01*"]
       assert_parsed(cell, expected, context)
     end
+  end
 
-    test "I_INFECT_PARASIT: A|B", context do
-      cell = "A|B"
-      expected = ["A00-B99"]
-      assert_parsed(cell, expected, context)
-    end
-
-    test "VI_NERVOUS: G", context do
-      cell = "G"
-      expected = ["G00-G99"]
-      assert_parsed(cell, expected, context)
-    end
-
-    test "XIII_MUSCULOSKELET: M", context do
-      cell = "M"
-      expected = ["M00-M99"]
-      assert_parsed(cell, expected, context)
-    end
-
-    test "Symptom pairs & ➝ +: T58&F0289", context do
-      # From ST19_DEMEN_RELATED_TOXIC_EFFECT_CARBON_MONOX
-      # Interpret "&" as a symptom pair, so either "+" or "*".
-      cell = "T58&F0289"
-      expected = ["T58+F02.89"]
-      assert_parsed(cell, expected, context)
-    end
-
-    test "Symptom pairs & ➝ *: G590&E104|G590&E114", context do
-      # From DM_MONONEURO
-      cell = "G590&E104|G590&E114"
-      expected = ["G59.0*E10.4", "G59.0*E11.4"]
-      assert_parsed(cell, expected, context)
-    end
-
-    test "Symptom pairs & ➝ * :  M07&L405|M07[1-3]", context do
-      # From M13_PSORIARTH_ICD10
-      cell = "M07[0-3]&L405|M07[1-3]"
-      expected = [
-	"M07.0*L40.5",
-	"M07.1*L40.5",
-	"M07.2*L40.5",
-	"M07.3*L40.5",
-	"M07.1*",
-	"M07.2*",
-	"M07.3*"
-      ]
-      assert_parsed(cell, expected, context)
-    end
-
-    test "Symptom pairs non-matching: F009&G309", context do
-      # From AD_U
-      #
-      # This is a tricky case. The pair F00.9, G30.9 is not found in
-      # the official Finnish ICD-10 document, but the pair appears
-      # however in the FinnGen data.
-      # Best course of action is to not match anything on this pair so
-      # it is displayed as is in Risteys.
-      cell = "F009&G309"
-      expected = []
-      assert_parsed(cell, expected, context)
-    end
-
-    test "Mode: %J300", context do
+  describe "Mode endpoints" do
+    test "%J300", context do
       # From RHINITIS_VASOMOTOR
       cell = "%J300"
       expected = ["J30.0"]
       assert_parsed(cell, expected, context)
     end
 
-    test "Mode: %G40[0-1]", context do
+    test "%G40[0-1]", context do
       # From GE_MODE
       cell = "%G40[0-1]"
       expected = ["G40.0", "G40.1"]
       assert_parsed(cell, expected, context)
     end
 
-    test "Mode: %J45|J46", context do
+    test "%J45|J46", context do
       # From ASTHMA_MODE
       cell = "%J45|J46"
       expected = ["J45", "J46"]
