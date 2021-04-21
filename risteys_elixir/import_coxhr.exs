e7f66ccd907ae12d81c7fbc836630c1e01104b78
Vincent <vincent-octo@users.noreply.github.com>
2021-04-21 11:18:08 +0000

    risteys: remove bch stats (+db migration)

--- risteys_elixir/import_coxhr.exs
+++ risteys_elixir/import_coxhr.exs
@@ -8,44 +8,11 @@
 # - prior
 # - outcome
 # - lag_hr
-# - step_size
 # - nindivs_prior_outcome
-# - absolute_risk
-# - prior_coef
-# - prior_se
 # - prior_hr
 # - prior_ci_lower
 # - prior_ci_upper
 # - prior_pval
-# - prior_zval
-# - prior_norm_mean
-# - year_coef
-# - year_se
-# - year_hr
-# - year_ci_lower
-# - year_ci_upper
-# - year_pval
-# - year_zval
-# - year_norm_mean
-# - sex_coef
-# - sex_se
-# - sex_hr
-# - sex_ci_lower
-# - sex_ci_upper
-# - sex_pval
-# - sex_zval
-# - sex_norm_mean
-# - bch
-# - bch_0
-# - bch_2.5
-# - bch_5
-# - bch_7.5
-# - bch_10
-# - bch_12.5
-# - bch_15
-# - bch_17.5
-# - bch_20
-# - bch_21.99
 
 alias Risteys.{Repo, CoxHR, Phenocode}
 import Ecto.Query
@@ -83,23 +50,7 @@ end)
                    "prior_ci_lower" => ci_min,
                    "prior_ci_upper" => ci_max,
                    "prior_pval" => pvalue,
-                   "nindivs_prior_outcome" => n_individuals,
-                   "prior_coef" => prior_coef,
-                   "prior_norm_mean" => prior_norm_mean,
-                   "year_coef" => year_coef,
-                   "year_norm_mean" => year_norm_mean,
-                   "sex_coef" => sex_coef,
-                   "sex_norm_mean" => sex_norm_mean,
-                   "bch_0" => bch_year_0,
-                   "bch_2.5" => bch_year_2p5,
-                   "bch_5" => bch_year_5,
-                   "bch_7.5" => bch_year_7p5,
-                   "bch_10" => bch_year_10,
-                   "bch_12.5" => bch_year_12p5,
-                   "bch_15" => bch_year_15,
-                   "bch_17.5" => bch_year_17p5,
-                   "bch_20" => bch_year_20,
-                   "bch_21.99" => bch_year_21p99
+                   "nindivs_prior_outcome" => n_individuals
                  }, idx} ->
   Logger.debug("Processing pair: #{prior} -> #{outcome}")
 
@@ -114,9 +65,6 @@ end)
     else
       lagged_years |> String.to_integer() |> trunc
     end
-
-  sex_coef = if sex_coef == "nan", do: nil, else: String.to_float(sex_coef)
-  sex_norm_mean = if sex_norm_mean == "nan", do: nil, else: String.to_float(sex_norm_mean)
 
   cond do
     hr == "nan" ->
@@ -147,23 +95,7 @@ end)
           ci_min: String.to_float(ci_min),
           ci_max: String.to_float(ci_max),
           pvalue: String.to_float(pvalue),
-          n_individuals: String.to_integer(n_individuals),
-          prior_coef: String.to_float(prior_coef),
-          prior_norm_mean: String.to_float(prior_norm_mean),
-          year_coef: String.to_float(year_coef),
-          year_norm_mean: String.to_float(year_norm_mean),
-          sex_coef: sex_coef,
-          sex_norm_mean: sex_norm_mean,
-          bch_year_0: String.to_float(bch_year_0),
-          bch_year_2p5: String.to_float(bch_year_2p5),
-          bch_year_5: String.to_float(bch_year_5),
-          bch_year_7p5: String.to_float(bch_year_7p5),
-          bch_year_10: String.to_float(bch_year_10),
-          bch_year_12p5: String.to_float(bch_year_12p5),
-          bch_year_15: String.to_float(bch_year_15),
-          bch_year_17p5: String.to_float(bch_year_17p5),
-          bch_year_20: String.to_float(bch_year_20),
-          bch_year_21p99: String.to_float(bch_year_21p99)
+          n_individuals: String.to_integer(n_individuals)
         })
         |> Repo.insert_or_update()
 
