e8f2de9a23c1b2473807185c4a1d2ed14e9f88ec
essiviippola <essi.viippola@live.fi>
2021-11-26 14:52:46 +0200

    Implement loading FinRegistry data

---
+++ pipeline/risteys_pipeline/finregistry/load_data.py
@@ -0,0 +1,22 @@
+"""Functions for loading the FinRegistry datasets """
+
+import pandas as pd
+from risteys_pipeline.config import FINREGISTRY_ENDPOINTS_DATA_PATH, FINREGISTRY_FIRST_EVENTS_DATA_PATH, FINREGISTRY_MINIMAL_PHENOTYPE_DATA_PATH
+
+def load_minimal_phenotype_data(data_path=FINREGISTRY_MINIMAL_PHENOTYPE_DATA_PATH):
+    """Loads minimal phenotype data as a data frame."""
+    cols = ["FINREGISTRYID", "date_of_birth", "sex"]
+    df = pd.read_csv(data_path, usecols=cols, header=0, sep=",")
+    return(df)
+
+def load_first_events_data(data_path=FINREGISTRY_FIRST_EVENTS_DATA_PATH):
+    """Loads minimal phenotype data as a data frame."""
+    df = pd.read_csv(data_path, header=0, sep=",")
+    return(df)
+
+def load_endpoints_data(data_path=FINREGISTRY_ENDPOINTS_DATA_PATH):
+    """Loads endpoints data as a data frame."""
+    # TODO: replace excel with csv
+    cols = ["NAME", "SEX"]
+    df = pd.read_excel(data_path, sheet_name='Sheet 1', usecols=cols)
+    return(df)
