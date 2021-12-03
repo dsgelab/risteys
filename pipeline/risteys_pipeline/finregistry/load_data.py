fe76b03b833f165132769adbb6f22e37f1f49c71
essiviippola <essi.viippola@live.fi>
2021-12-03 12:43:04 +0200

    black code formatting

--- pipeline/risteys_pipeline/finregistry/load_data.py
+++ pipeline/risteys_pipeline/finregistry/load_data.py
@@ -1,22 +1,26 @@
-"""Functions for loading the FinRegistry datasets """
+"""Functions for loading the FinRegistry datasets"""
 
 import pandas as pd
-from risteys_pipeline.config import FINREGISTRY_ENDPOINTS_DATA_PATH, FINREGISTRY_FIRST_EVENTS_DATA_PATH, FINREGISTRY_MINIMAL_PHENOTYPE_DATA_PATH
+from risteys_pipeline.config import *
+
 
 def load_minimal_phenotype_data(data_path=FINREGISTRY_MINIMAL_PHENOTYPE_DATA_PATH):
     """Loads minimal phenotype data as a data frame."""
     cols = ["FINREGISTRYID", "date_of_birth", "sex"]
     df = pd.read_csv(data_path, usecols=cols, header=0, sep=",")
-    return(df)
+    return df
+
 
 def load_first_events_data(data_path=FINREGISTRY_FIRST_EVENTS_DATA_PATH):
     """Loads minimal phenotype data as a data frame."""
     df = pd.read_csv(data_path, header=0, sep=",")
-    return(df)
+    return df
+
 
 def load_endpoints_data(data_path=FINREGISTRY_ENDPOINTS_DATA_PATH):
     """Loads endpoints data as a data frame."""
     # TODO: replace excel with csv
     cols = ["NAME", "SEX"]
-    df = pd.read_excel(data_path, sheet_name='Sheet 1', usecols=cols)
-    return(df)
+    df = pd.read_excel(data_path, sheet_name="Sheet 1", usecols=cols)
+    return df
+
