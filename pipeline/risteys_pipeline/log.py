0fc4d0db8a8baf9ee197544fc90880fad76ded09
essiviippola <essi.viippola@live.fi>
2021-12-09 20:29:15 +0200

    Log name of the module

--- pipeline/risteys_pipeline/log.py
+++ pipeline/risteys_pipeline/log.py
@@ -1,9 +1,9 @@
 import logging
 from os import getenv
 
+logger = logging.getLogger(__name__)
 
 level = getenv("LOG_LEVEL", logging.INFO)
-logger = logging.getLogger("pipeline")
 formatter = logging.Formatter(
     "%(asctime)s %(levelname)-8s %(module)-21s %(funcName)-25s: %(message)s"
 )
