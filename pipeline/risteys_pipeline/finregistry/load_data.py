1890dace0febf527963896303bbeba97d912596f
essiviippola <essi.viippola@live.fi>
2021-12-09 20:52:58 +0200

    Add logs

--- pipeline/risteys_pipeline/finregistry/load_data.py
+++ pipeline/risteys_pipeline/finregistry/load_data.py
@@ -7,6 +7,7 @@ from risteys_pipeline.log import logger
 
 def load_minimal_phenotype_data(data_path=FINREGISTRY_MINIMAL_PHENOTYPE_DATA_PATH):
     """Loads minimal phenotype data as a data frame."""
+    logger.info("Loading minimal phenotype data")
     cols = ["FINREGISTRYID", "date_of_birth", "sex"]
     df = pd.read_csv(data_path, usecols=cols, header=0, sep=",")
     return df
@@ -14,6 +15,7 @@ def load_minimal_phenotype_data(data_path=FINREGISTRY_MINIMAL_PHENOTYPE_DATA_PAT
 
 def load_first_events_data(data_path=FINREGISTRY_FIRST_EVENTS_DATA_PATH):
     """Loads minimal phenotype data as a data frame."""
+    logger.info("Loading first events data")
     cols = ["FINNGENID", "ENDPOINT", "AGE", "YEAR", "NEVT"]
     df = pd.read_csv(data_path, header=0, sep=",", usecols=cols)
     return df
@@ -22,6 +24,7 @@ def load_first_events_data(data_path=FINREGISTRY_FIRST_EVENTS_DATA_PATH):
 def load_endpoints_data(data_path=FINREGISTRY_ENDPOINTS_DATA_PATH):
     """Loads endpoints data as a data frame."""
     # TODO: replace excel with csv for speed
+    logger.info("Loading endpoints data")
     cols = ["NAME", "SEX", "OMIT"]
     df = pd.read_excel(data_path, sheet_name="Sheet 1", usecols=cols, header=0)
     return df
