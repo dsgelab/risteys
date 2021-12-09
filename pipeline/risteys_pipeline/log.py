36c795c5bd0ff6f6670de80f6113aa0c9d0f1760
essiviippola <essi.viippola@live.fi>
2021-12-09 20:26:36 +0200

    Change stream handler to file handler

--- pipeline/risteys_pipeline/log.py
+++ pipeline/risteys_pipeline/log.py
@@ -4,10 +4,11 @@ from os import getenv
 
 level = getenv("LOG_LEVEL", logging.INFO)
 logger = logging.getLogger("pipeline")
-handler = logging.StreamHandler()
 formatter = logging.Formatter(
     "%(asctime)s %(levelname)-8s %(module)-21s %(funcName)-25s: %(message)s"
 )
+# handler = logging.StreamHandler()
+handler = logging.FileHandler("risteys.log")
 
 handler.setFormatter(formatter)
 logger.addHandler(handler)
