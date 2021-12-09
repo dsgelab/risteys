30d8b44eff322519d854b08d2e687f62e01ec093
essiviippola <essi.viippola@live.fi>
2021-12-09 20:00:07 +0200

    Black formatting

--- pipeline/risteys_pipeline/log.py
+++ pipeline/risteys_pipeline/log.py
@@ -6,7 +6,8 @@ level = getenv("LOG_LEVEL", logging.INFO)
 logger = logging.getLogger("pipeline")
 handler = logging.StreamHandler()
 formatter = logging.Formatter(
-    "%(asctime)s %(levelname)-8s %(module)-21s %(funcName)-25s: %(message)s")
+    "%(asctime)s %(levelname)-8s %(module)-21s %(funcName)-25s: %(message)s"
+)
 
 handler.setFormatter(formatter)
 logger.addHandler(handler)
