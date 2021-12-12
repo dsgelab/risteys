6215f1a193858d302eb0c337354eea9e62007888
essiviippola <essi.viippola@live.fi>
2021-12-12 12:48:55 +0200

    Add option to preprocess data during import

--- pipeline/main.py
+++ pipeline/main.py
@@ -9,24 +9,11 @@ from risteys_pipeline.finregistry.load_data import (
     load_minimal_phenotype_data,
     load_wide_first_events_data,
 )
-from risteys_pipeline.finregistry.preprocess_data import (
-    list_excluded_subjects,
-    preprocess_endpoints_data,
-    preprocess_minimal_phenotype_data,
-    preprocess_wide_first_events_data,
-)
 from risteys_pipeline.finregistry.sample import sample_cases_and_controls
 
-# Load data
-endpoints = load_endpoints_data()
-minimal_phenotype = load_minimal_phenotype_data()
-
-# Preprocess minimal phenotype and endpoint definitions data
-excluded_subjects = list_excluded_subjects(minimal_phenotype)
-endpoints = preprocess_endpoints_data(endpoints)
-minimal_phenotype = preprocess_minimal_phenotype_data(
-    minimal_phenotype, excluded_subjects
-)
+# Load and preprocess endpoint definitions and minimal phenotype data
+endpoints = load_endpoints_data(preprocess=True)
+minimal_phenotype = load_minimal_phenotype_data(preprocess=True)
 
 # Set up outcomes and exposures
 outcomes = ["DEATH"]
@@ -35,17 +22,16 @@ exposures = ["T2D", "5_SCHZPHR"]
 # Loop through outcomes and exposures
 
 for outcome, exposure in product(outcomes, exposures):
-    # Load first events data
-    first_events = load_wide_first_events_data(exposure, outcome)
-    first_events = preprocess_wide_first_events_data(first_events)
+    # Load and preprocess first events data
+    first_events = load_wide_first_events_data(exposure, outcome, preprocess=True)
 
     # Merge minimal phenotype with first events
-    # Only subjects in minimal phenotype are included
+    # Note: only subjects in minimal phenotype are included
     df = minimal_phenotype.merge(first_events, how="left", on="finregistryid")
 
     # Sample cases and controls based on outcome
     caseids, controlids = sample_cases_and_controls(
-        outcome, excluded_subjects, n_cases=250000, controls_per_case=2
+        df, n_cases=250000, controls_per_case=2
     )
     df = df.loc[df["finregistryid"].isin(caseids + controlids), :].reset_index()
 
