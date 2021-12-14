7059205839e37b0df36fb4ccaaf5c47a4370da0f
essiviippola <essi.viippola@live.fi>
2021-12-14 08:19:24 +0200

    Replace os with pathlib
    For readability

--- pipeline/risteys_pipeline/config.py
+++ pipeline/risteys_pipeline/config.py
@@ -1,20 +1,22 @@
-import os
+from pathlib import Path
 
 # Input data
-ROOT_DIR = os.path.join("data", "processed_data")
 
-FINREGISTRY_MINIMAL_PHENOTYPE_DATA_PATH = os.path.join(
-    ROOT_DIR, "notebooks", "mpf", "minimal_phenotype_file.csv"
+ROOT_DIR = Path("/data") / "processed_data"
+
+FINREGISTRY_MINIMAL_PHENOTYPE_DATA_PATH = (
+    ROOT_DIR / "notebooks" / "mpf" / "minimal_phenotype_file.csv"
 )
 
-FINREGISTRY_WIDE_FIRST_EVENTS_DATA_PATH = os.path.join(
-    ROOT_DIR, "endpointer", "main", "finngen_endpoints_04-09-2021_v3.txt.ALL"
+FINREGISTRY_WIDE_FIRST_EVENTS_DATA_PATH = (
+    ROOT_DIR / "endpointer" / "main" / "finngen_endpoints_04-09-2021_v3.txt.ALL"
 )
 
-FINREGISTRY_ENDPOINTS_DATA_PATH = os.path.join(
-    ROOT_DIR, "endpointer", "main", "FINNGEN_ENDPOINTS_DF8_Final_2021-06-21.xlsx"
+FINREGISTRY_ENDPOINTS_DATA_PATH = (
+    ROOT_DIR / "endpointer" / "main" / "FINNGEN_ENDPOINTS_DF8_Final_2021-06-21.xlsx"
 )
 
 # Constants
+
 FOLLOWUP_START = 1998.0
 FOLLOWUP_END = 2020.99
