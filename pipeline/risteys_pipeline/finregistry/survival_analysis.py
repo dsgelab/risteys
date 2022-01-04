a3e1ac88772a3a5aba44e91c8dbcee64df4380e3
essiviippola <essi.viippola@live.fi>
2022-01-04 11:06:27 +0200

    Handle timescale in survival analysis instead of build_cph_dataset

--- pipeline/risteys_pipeline/finregistry/survival_analysis.py
+++ pipeline/risteys_pipeline/finregistry/survival_analysis.py
@@ -105,19 +105,19 @@ def build_exposure_dataset(df):
     return exposure
 
 
-def build_cph_dataset(df, timescale):
+def build_cph_dataset(df):
     """
-    Build the dataset for survival analysis using time-on-study or age as timescale.
+    Build the dataset for survival analysis. 
+    Both time-on-study and age can be used as a timescale with the resulting dataframe.
     Exposure is modeled as a time-varying covariate.
 
     Args:
         df (DataFrame): dataframe with the following columns:
         finregistryid, birth_year, death_year, exposure_year, outcome_year, weight, female
-        timescale (bool): timescale for Cox regression, either time-on-study or age
 
     Returns:
         res (DataFrame): a dataframe with the following columns: 
-        start, stop, outcome, exposure, birth_year, weight, female
+        start_time, stop_time, start_age, stop_age, outcome, exposure, birth_year, weight, female
 
     """
     # Copy dataframe
@@ -139,17 +139,19 @@ def build_cph_dataset(df, timescale):
         event_col="outcome",
     )
 
-    # Change time-on-study to age if timescale is age
-    if timescale == "age":
-        res["start"] = res["start"] - res["birth_year"] + FOLLOWUP_START
-        res["stop"] = res["stop"] - res["birth_year"] + FOLLOWUP_START
+    # Rename start and stop columns
+    res = res.rename(columns={"start": "start_time", "stop": "stop_time"})
+
+    # Add start and stop age (for age as timescale)
+    res["start_age"] = res["start_time"] - res["birth_year"] + FOLLOWUP_START
+    res["stop_age"] = res["stop_time"] - res["birth_year"] + FOLLOWUP_START
 
     # Add exposure if missing
     if "exposure" not in res:
         res["exposure"] = np.nan
 
     # Drop rows where start >= stop
-    start_after_stop = res["start"] >= res["stop"]
+    start_after_stop = res["start_time"] >= res["stop_time"]
     res = res.loc[~start_after_stop].reset_index(drop=True)
     logger.info(f"{sum(start_after_stop)} rows had start >= stop")
 
@@ -160,7 +162,7 @@ def build_cph_dataset(df, timescale):
     return res
 
 
-def survival_analysis(df_cph):
+def survival_analysis(df_cph, timescale):
     """
     Survival/mortality analysis with time-on-study or age as timescale.
     Analysis is only run if there's more than MIN_SUBJECTS subjects in exposed and unexposed cases and controls.
@@ -171,8 +173,6 @@ def survival_analysis(df_cph):
     Returns:
         cph (Object): fitted Cox Proportional Hazard model object. None if there aren't enough subjects.
     """
-    # Check that there's enough subjects in each cell
-    # Unique finregistryids are counted, so the last char representing the case/control group is dropped
     min_subjects_check = (
         pd.crosstab(
             df_cph["outcome"],
@@ -184,13 +184,23 @@ def survival_analysis(df_cph):
     )
 
     if min_subjects_check:
+
+        if timescale == "age":
+            entry_col = "start_age"
+            duration_col = "stop_age"
+            df_cph = df_cph.drop(columns=["start_time", "stop_time"])
+        else:
+            entry_col = "start_time"
+            duration_col = "stop_time"
+            df_cph = df_cph.drop(columns=["start_age", "stop_age"])
+
         logger.info("Fitting Cox PH model")
         df_cph = df_cph.drop("finregistryid", axis=1)
         cph = CoxPHFitter()
         cph.fit(
             df_cph,
-            entry_col="start",
-            duration_col="stop",
+            entry_col=entry_col,
+            duration_col=duration_col,
             event_col="outcome",
             weights_col="weight",
             robust=True,
