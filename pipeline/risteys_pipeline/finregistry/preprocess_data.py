d617f2af16077137f3f04c17edb25818eed1ec30
essiviippola <essi.viippola@live.fi>
2022-02-10 10:09:43 +0200

    Move minimal phenotype preprocessing to load_data

--- pipeline/risteys_pipeline/finregistry/preprocess_data.py
+++ pipeline/risteys_pipeline/finregistry/preprocess_data.py
@@ -35,38 +35,6 @@ def list_excluded_subjects(minimal_phenotype):
     return excluded_subjects
 
 
-def preprocess_minimal_phenotype_data(df):
-    """Applies the following preprocessing steps to minimal phenotype data:
-        - lowercase column names 
-        - drop duplicated rows
-        - add birth and death year
-        - drop excluded subjects
-        - add indicator for females (bool)
-
-    Args:
-        df (DataFrame): minimal phenotype dataframe
-
-    Returns:
-        df (DataFrame): minimal phenotype dataframe with the following columns: 
-        finregistryid, date_of_birth, death_date, sex, death_age, dead, female
-    """
-    df = df.drop_duplicates(subset=["finregistryid"]).reset_index(drop=True)
-    df["birth_year"] = (
-        df["date_of_birth"].dt.year
-        + (df["date_of_birth"].dt.dayofyear - 1) / DAYS_IN_YEAR
-    )
-    df["death_year"] = (
-        df["death_date"].dt.year + (df["death_date"].dt.dayofyear - 1) / DAYS_IN_YEAR
-    )
-    df["female"] = df["sex"] == SEX_FEMALE
-    excluded_subjects = list_excluded_subjects(df)
-    df = df.loc[~df["finregistryid"].isin(excluded_subjects)]
-
-    logger.info(f"{df.shape[0]} rows after data pre-processing")
-
-    return df
-
-
 def merge_first_events_data_with_minimal_phenotype(first_events, minimal_phenotype):
     """Merge first events data with minimal phenotype"""
     first_events = first_events.merge(minimal_phenotype, how="left", on="finregistryid")
