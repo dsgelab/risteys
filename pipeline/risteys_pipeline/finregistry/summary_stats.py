972939b014d5249e9b56bf75eff7d016089e1281
essiviippola <essi.viippola@live.fi>
2022-02-10 17:24:15 +0200

    Refactor key figures

--- pipeline/risteys_pipeline/finregistry/summary_stats.py
+++ pipeline/risteys_pipeline/finregistry/summary_stats.py
@@ -1,28 +1,27 @@
-"""Functions for summary statistics"""
+"""
+Functions for the following summary statistics:
+- key figures (number of individuals, unadjusted prevalence, mean age at first event)
+- distributions: age at first event, year at first event
+"""
 
 import numpy as np
+import pandas as pd
 from risteys_pipeline.log import logger
-from risteys_pipeline.config import (
-    FOLLOWUP_START,
-    FOLLOWUP_END,
-    MIN_SUBJECTS_PERSONAL_DATA,
-)
-
-SEX_FEMALE = 1.0
-SEX_MALE = 0.0
+from risteys_pipeline.config import MIN_SUBJECTS_PERSONAL_DATA
 
 
-def compute_key_figures(first_events):
-    """Compute the following key figures for each endpoint:
+def compute_key_figures(first_events, minimal_phenotype):
+    """
+    Compute the following key figures for each endpoint:
         - number of individuals
         - unadjusted prevalence (%)
         - mean age at first event (years)
 
-    The numbers are calculated for men, women, and all separately.
-    Note: the total number of individuals is calculated from first_events and not minimal_phenotype.
+    The numbers are calculated for males, females, and all.
 
     Args:
-        first_events (DataFrame): preprocessed first events dataframe
+        first_events (DataFrame): first events dataframe
+        minimal_phenotype(DataFrame): minimal phenotype dataframe
 
     Returns:
         key_figures (DataFrame): key figures dataframe with the following columns:
@@ -31,61 +30,61 @@ def compute_key_figures(first_events):
         mean_age_female, mean_age_male, mean_age_all,
         prevalence_female, prevalence_male, prevalence_all
     """
+    # Calculate the total number of individuals
+    # Note: individuals for sex="unknown" is based on first events
+    n_total = {
+        "female": sum(minimal_phenotype["female"]),
+        "male": sum(~minimal_phenotype["female"]),
+        "unknown": sum(first_events["sex"] == "unknown"),
+    }
 
-    # Compute number of individuals and mean age
+    # Calculate key figures by endpoint and sex
     key_figures = (
-        first_events.replace(
-            {"sex": {SEX_FEMALE: "female", SEX_MALE: "male", np.nan: "unknown"}}
-        )
-        .groupby(["endpoint", "sex"])
+        first_events.groupby(["endpoint", "sex"])
         .agg({"finregistryid": "count", "age": "mean"})
         .rename(columns={"finregistryid": "nindivs_", "age": "mean_age_"})
         .fillna({"nindivs_": 0})
         .reset_index()
-        .pivot(index="endpoint", columns="sex")
+    )
+    key_figures["n_total"] = key_figures["sex"].replace(n_total)
+    key_figures["prevalence_"] = key_figures["nindivs_"] / key_figures["n_total"]
+    key_figures["weight"] = key_figures["n_total"] / sum(n_total.values())
+
+    # Calculate key figures by endpoint for all individuals
+    key_figures_all = (
+        key_figures.groupby("endpoint")
+        .agg(
+            {
+                "nindivs_": "sum",
+                "mean_age_": lambda x: np.average(
+                    x, weights=key_figures.loc[x.index, "weight"]
+                ),
+                "prevalence_": lambda x: np.average(
+                    x, weights=key_figures.loc[x.index, "weight"]
+                ),
+            }
+        )
         .reset_index()
+        .assign(sex="all")
     )
 
-    # Flatten hierarchical columns
-    key_figures.columns = ["".join(col).strip() for col in key_figures.columns.values]
+    # Drop rows with sex=unknown
+    key_figures = key_figures.loc[key_figures["sex"] != "unknown"]
+    key_figures = key_figures.reset_index(drop=True)
 
-    # Calculte the total number of individuals
-    n_all = first_events["finregistryid"].unique().shape[0]
-    n_females = (
-        first_events.loc[first_events["sex"] == SEX_FEMALE, "finregistryid"]
-        .unique()
-        .shape[0]
-    )
-    n_males = (
-        first_events.loc[first_events["sex"] == SEX_MALE, "finregistryid"]
-        .unique()
-        .shape[0]
-    )
-
-    # Calculate nindivs_all
-    key_figures["nindivs_all"] = key_figures[
-        ["nindivs_female", "nindivs_male", "nindivs_unknown"]
-    ].sum(axis=1, skipna=True)
-
-    # Calculate prevalence
-    key_figures["prevalence_all"] = key_figures["nindivs_all"] / n_all
-    key_figures["prevalence_female"] = key_figures["nindivs_female"] / n_females
-    key_figures["prevalence_male"] = key_figures["nindivs_male"] / n_males
-
-    # Calculate the mean age for all individuals as a weighted mean
-    key_figures["mean_age_all"] = (
-        key_figures["nindivs_female"] * key_figures["mean_age_female"]
-        + key_figures["nindivs_male"] * key_figures["mean_age_male"]
-        + key_figures["nindivs_unknown"] * key_figures["mean_age_unknown"]
-    ) / key_figures["nindivs_all"]
+    # Combine the two datasets
+    key_figures = pd.concat([key_figures, key_figures_all])
 
     # Drop redundant columns
-    key_figures = key_figures.drop(columns=["nindivs_unknown", "mean_age_unknown"])
+    key_figures = key_figures.drop(columns=["weight", "n_total"])
 
-    # Drop personal data
-    for group in ["female", "male", "all"]:
-        cols = [x + group for x in ["nindivs_", "mean_age_", "prevalence_"]]
-        indx = key_figures["nindivs_" + group] <= MIN_SUBJECTS_PERSONAL_DATA
-        key_figures.loc[indx, cols] = np.nan
+    # Remove personal data
+    indx = key_figures["nindivs_"] <= MIN_SUBJECTS_PERSONAL_DATA
+    cols = ["nindivs_", "mean_age_", "prevalence_"]
+    key_figures.loc[indx, cols] = np.nan
+
+    # Pivot and flatten hierarchical columns
+    key_figures = key_figures.pivot(index="endpoint", columns="sex").reset_index()
+    key_figures.columns = ["".join(col).strip() for col in key_figures.columns.values]
 
     return key_figures
