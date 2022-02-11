05ecb1366ebce4ffe7d7287a322af267893a22a9
essiviippola <essi.viippola@live.fi>
2022-02-11 07:47:39 +0200

    Remove sample cases and controls as obsolete
    Long first events dataset has a different structure than the wide first events

--- pipeline/risteys_pipeline/finregistry/sample.py
+++ pipeline/risteys_pipeline/finregistry/sample.py
@@ -1,61 +1,7 @@
 """Functions for sampling the data"""
 
 import numpy as np
-import pandas as pd
-from random import sample
 from risteys_pipeline.log import logger
-from risteys_pipeline.config import FOLLOWUP_START, FOLLOWUP_END
-
-
-def sample_cases_and_controls(df, n_cases=250000, controls_per_case=2):
-    """Samples df and adds case-cohort weights (`weight`) and indicators for cases/controls (`case`).
-    
-    Args:
-        df (DataFrame): dataframe with the following columns: outcome_year, finregistryid
-        n_cases (int, optional): maximum number of cases to include
-        controls_per_case (int, optional): number of controls to include per case
-
-    Returns: 
-        df_sample (DataFrame): sampled dataframe
-    """
-
-    caseids = df.loc[
-        df["outcome_year"].between(FOLLOWUP_START, FOLLOWUP_END), "finregistryid"
-    ].tolist()
-    controlids = df["finregistryid"].tolist()
-
-    if n_cases > len(caseids):
-        logger.info(f"Requested {n_cases} cases but found {len(caseids)}")
-        n_cases = len(caseids)
-
-    n_controls = round(n_cases * controls_per_case)
-
-    if n_controls > len(controlids):
-        logger.info(f"Requested {n_controls} controls but found {len(controlids)}")
-        n_controls = len(controlids)
-
-    sample_of_caseids = sample(caseids, n_cases)
-    sample_of_controlids = sample(controlids, n_controls)
-
-    weight_cases, weight_controls = calculate_case_cohort_weights(
-        caseids, controlids, sample_of_caseids, sample_of_controlids
-    )
-
-    df_sample_cases = df.loc[df["finregistryid"].isin(sample_of_caseids)].copy()
-    df_sample_cases = df_sample_cases.reset_index(drop=True)
-    df_sample_cases["case"] = 1
-    df_sample_cases["weight"] = weight_cases
-
-    df_sample_controls = df.loc[df["finregistryid"].isin(sample_of_controlids)].copy()
-    df_sample_controls = df_sample_controls.reset_index(drop=True)
-    df_sample_controls["case"] = 0
-    df_sample_controls["weight"] = weight_controls
-
-    df_sample = pd.concat([df_sample_cases, df_sample_controls], axis=0)
-    df_sample = df_sample.reset_index(drop=True)
-
-    return df_sample
-
 
 def calculate_case_cohort_weights(
     caseids, controlids, sample_of_caseids, sample_of_controlids
