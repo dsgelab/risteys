1b18838c03cec677a414e29221fdd30432cf11a1
essiviippola <essi.viippola@live.fi>
2022-02-15 19:23:03 +0200

    finregistry-pipeline: Add cumulative incidence

--- pipeline/risteys_pipeline/finregistry/summary_stats.py
+++ pipeline/risteys_pipeline/finregistry/summary_stats.py
@@ -2,12 +2,17 @@
 Functions for the following summary statistics:
 - key figures (number of individuals, unadjusted prevalence, mean age at first event)
 - distributions: age at first event, year at first event
+- cumulative incidence
 """
 
 import numpy as np
 import pandas as pd
 from risteys_pipeline.log import logger
 from risteys_pipeline.config import MIN_SUBJECTS_PERSONAL_DATA
+from risteys_pipeline.finregistry.survival_analysis import (
+    build_cph_dataset,
+    survival_analysis,
+)
 
 
 def compute_key_figures(first_events, minimal_phenotype):
@@ -88,3 +93,68 @@ def compute_key_figures(first_events, minimal_phenotype):
     key_figures.columns = ["".join(col).strip() for col in key_figures.columns.values]
 
     return key_figures
+
+
+def cumulative_incidence(cohort, all_cases, endpoints):
+    """
+    Cumulative incidence with age as timescale stratified by sex
+
+    Args:
+        minimal_phenotype (DataFrame): minimal phenotype dataset
+        first_events (DataFrame): first events dataset
+        endpoints (DataFrame): endpoint definition dataset
+b
+    Returns:
+        result (DataFrame): dataset with the following columns:
+            endpoint: the name of the endpoint
+            bch: baseline cumulative hazard by age group and sex
+            params: coefficients
+    """
+
+    n_endpoints = endpoints.shape[0]
+    result = pd.DataFrame(index=endpoints["endpoint"], columns=["bch", "params"])
+
+    for i, row in endpoints.iterrows():
+
+        outcome, female = row
+
+        # Fit the Cox PH model
+        logger.info(f"Outcome {i+1}/{n_endpoints}: {outcome}")
+        if pd.isnull(female):
+            df_cph = build_cph_dataset(outcome, None, cohort, all_cases)
+            cph = survival_analysis(df_cph, "age", stratify_by_sex=True)
+        else:
+            subcohort = cohort.loc[cohort["female"] == female]
+            subcohort = subcohort.reset_index(drop=True)
+            df_cph = build_cph_dataset(outcome, None, subcohort, all_cases)
+            cph = survival_analysis(df_cph, "age", drop_sex=True)
+
+        bch = np.nan
+        params = np.nan
+
+        if cph:
+            # Calculate number of events by age group
+            counts = df_cph.loc[df_cph["outcome"] == 1].reset_index(drop=True)
+            counts["age"] = round((counts["stop"] - counts["birth_year"]) / 10) * 10
+            counts = counts.groupby("age")["outcome"].sum().reset_index()
+            counts = counts.rename(columns={"outcome": "n_events"})
+
+            # Calculate baseline cumulative hazard by age group
+            bch = cph.baseline_cumulative_hazard_
+            bch = bch.reset_index()
+            bch = bch.rename(columns={"index": "age", 0: "male", 1: "female"})
+            bch["age"] = round(bch["age"] / 10) * 10
+            bch = bch.groupby("age").mean()
+            bch = bch.merge(counts, on="age", how="left")
+            bch = bch.loc[bch["n_events"] > MIN_SUBJECTS_PERSONAL_DATA]
+            bch = bch.drop(columns=["n_events"])
+            bch = bch.set_index("age").to_dict()
+
+            # Extract parameters
+            params = cph.params_.to_dict()
+
+        # Add data to resuls
+        result.loc[outcome] = {"bch": bch, "params": params}
+
+    return result
+
--- pipeline/risteys_pipeline/finregistry/survival_analysis.py
+++ pipeline/risteys_pipeline/finregistry/survival_analysis.py
@@ -4,7 +4,11 @@ import pandas as pd
 from lifelines import CoxPHFitter
 from lifelines.utils import add_covariate_to_timeline
 from risteys_pipeline.log import logger
-from risteys_pipeline.config import MIN_SUBJECTS_PERSONAL_DATA
+from risteys_pipeline.config import (
+    FOLLOWUP_END,
+    FOLLOWUP_START,
+    MIN_SUBJECTS_PERSONAL_DATA,
+)
 from risteys_pipeline.finregistry.sample import (
     get_cases,
     get_controls,
@@ -17,7 +21,35 @@ CONTROLS_PER_CASE = 4
 MIN_SUBJECTS_SURVIVAL_ANALYSIS = 100
 
 
-def build_cph_dataset(outcome, exposure, cohort, first_events):
+def prep_all_cases(first_events, cohort):
+    """
+    Prep all cases for survival analysis
+    - drop endpoints outside study timeframe
+    - drop IDs not included in the cohort
+    
+    Args:
+        first_events (DataFrame): first events dataset
+        cohort (DataFrame): cohort dataset, output of get_cohort()
+
+    Returns:
+        all_cases (DataFrame)
+    """
+    logger.info("Prepping all cases")
+    all_cases = first_events.copy()
+
+    inside_timeframe = (all_cases["birth_year"] + all_cases["age"]).between(
+        FOLLOWUP_START, FOLLOWUP_END
+    )
+    all_cases = all_cases.loc[inside_timeframe].reset_index(drop=True)
+
+    cohortids = cohort["finregistryid"]
+    all_cases = all_cases.merge(cohortids, how="right", on="finregistryid")
+    all_cases = all_cases.reset_index(drop=True)
+
+    return all_cases
+
+
+def build_cph_dataset(outcome, exposure, cohort, all_cases):
     """
     Build a dataset for fitting a Cox PH model.
     Exposure is included as a time-varying covariate if present.
@@ -32,43 +64,49 @@ def build_cph_dataset(outcome, exposure, cohort, first_events):
         df_cph (DataFrame): dataset with the following columns:
         finregistryid, start (year), stop (year), exposure, outcome, birth_year, female, weight
     """
-    cases = get_cases(first_events, outcome, n_cases=N_CASES)
-    controls = get_controls(cohort, CONTROLS_PER_CASE * cases.shape[0])
+    cases = get_cases(all_cases, outcome, n_cases=N_CASES)
+    n_cases = cases.shape[0]
 
-    weight_cases, weight_controls = calculate_case_cohort_weights(
-        cases["finregistryid"],
-        controls["finregistryid"],
-        cases["finregistryid"],
-        cohort["finregistryid"],
-    )
-    cases["weight"] = weight_cases
-    controls["weight"] = weight_controls
+    df_cph = None
 
-    df_cph = pd.concat([cases, controls])
-    df_cph = df_cph.reset_index(drop=True)
+    if n_cases > 0:
+        controls = get_controls(cohort, CONTROLS_PER_CASE * n_cases)
 
-    if exposure:
-        df_cph["finregistryid_unique"] = (
-            df_cph["finregistryid"] + "_" + df_cph["outcome"].map(str)
+        weight_cases, weight_controls = calculate_case_cohort_weights(
+            cases["finregistryid"],
+            controls["finregistryid"],
+            cases["finregistryid"],
+            cohort["finregistryid"],
         )
-        exposed = get_exposed(first_events, exposure, cases)
-        exposed = exposed.merge(
-            df_cph[["finregistryid", "finregistryid_unique"]],
-            how="left",
-            on="finregistryid",
-        )
-        df_cph = add_covariate_to_timeline(
-            df_cph.drop(columns=["finregistryid"]),
-            exposed,
-            id_col="finregistryid_unique",
-            duration_col="duration",
-            event_col="outcome",
-        )
+        cases["weight"] = weight_cases
+        controls["weight"] = weight_controls
+
+        df_cph = pd.concat([cases, controls])
         df_cph = df_cph.reset_index(drop=True)
 
-    df_cph = df_cph.drop(columns=["death_year"])
-    df_cph = df_cph.loc[df_cph["start"] < df_cph["stop"]]
-    df_cph = df_cph.reset_index(drop=True)
+        if exposure:
+            df_cph["finregistryid_unique"] = (
+                df_cph["finregistryid"] + "_" + df_cph["outcome"].map(str)
+            )
+            exposed = get_exposed(all_cases, exposure, cases)
+            exposed = exposed.merge(
+                df_cph[["finregistryid", "finregistryid_unique"]],
+                how="left",
+                on="finregistryid",
+            )
+            df_cph = add_covariate_to_timeline(
+                df_cph.drop(columns=["finregistryid"]),
+                exposed,
+                id_col="finregistryid_unique",
+                duration_col="duration",
+                event_col="outcome",
+            )
+            df_cph = df_cph.reset_index(drop=True)
+
+        df_cph["female"] = df_cph["female"].astype(int)
+        df_cph = df_cph.drop(columns=["death_year"])
+        df_cph = df_cph.loc[df_cph["start"] < df_cph["stop"]]
+        df_cph = df_cph.reset_index(drop=True)
 
     return df_cph
 
@@ -99,7 +137,9 @@ def check_min_number_of_subjects(df_cph):
     return check
 
 
-def survival_analysis(df_cph, timescale="time-on-study"):
+def survival_analysis(
+    df_cph, timescale="time-on-study", drop_sex=False, stratify_by_sex=False
+):
     """
     Fit a Cox PH model to the data. 
     The model is only fitted if the requirement for the minimum number of participants is met.
@@ -107,41 +147,56 @@ def survival_analysis(df_cph, timescale="time-on-study"):
     Args: 
         df_cph (DataFrame): output of build_cph_dataset()
         timescale (str): "time-on-study" (default) or "age"
+        drop_sex (bool): should sex be dropped from covariates
+        stratify_by_age (bool): should the analysis be stratified by sex
 
     Returns: 
         cph (CoxPHFitter): fitted Cox PH model. None if there's not enough subjects.
     """
 
-    check = check_min_number_of_subjects(df_cph)
+    cph = None
 
-    if not check:
-        logger.info("Not enough subjects")
-        cph = None
-    else:
+    if df_cph is not None:
 
-        df_timescale = df_cph.copy()
+        check = check_min_number_of_subjects(df_cph)
 
-        if timescale == "time-on-study":
-            df_timescale["stop"] = df_timescale["stop"] - df_timescale["start"]
-            df_timescale = df_timescale.drop(columns=["start"])
-            entry_col = None
+        if not check:
+            logger.info("Not enough subjects")
+            cph = None
+        else:
+            df_timescale = df_cph.copy()
 
-        if timescale == "age":
-            df_timescale["start"] = df_timescale["start"] - df_timescale["birth_year"]
-            df_timescale["stop"] = df_timescale["stop"] - df_timescale["birth_year"]
-            entry_col = "start"
+            # Set timescale
+            if timescale == "time-on-study":
+                df_timescale["stop"] = df_timescale["stop"] - df_timescale["start"]
+                df_timescale = df_timescale.drop(columns=["start"])
+                entry_col = None
+            elif timescale == "age":
+                df_timescale["start"] = df_timescale["start"] - df_timescale["birth_year"]
+                df_timescale["stop"] = df_timescale["stop"] - df_timescale["birth_year"]
+                entry_col = "start"
 
-        df_timescale = df_timescale.drop(columns=["finregistryid"])
+            # Drop sex if specified
+            if drop_sex:
+                df_timescale = df_timescale.drop(columns=["female"])
 
-        logger.info("Fitting the Cox PH model")
-        cph = CoxPHFitter()
-        cph.fit(
-            df_timescale,
-            entry_col=entry_col,
-            duration_col="stop",
-            event_col="outcome",
-            weights_col="weight",
-            robust=True,
-        )
+            # Set strata if specified
+            strata = ["female"] if stratify_by_sex == True else None
+
+            # Drop FinRegistry IDs
+            df_timescale = df_timescale.drop(columns=["finregistryid"])
+
+            # Fit the model
+            logger.info("Fitting the Cox PH model")
+            cph = CoxPHFitter()
+            cph.fit(
+                df_timescale,
+                entry_col=entry_col,
+                duration_col="stop",
+                event_col="outcome",
+                strata=strata,
+                weights_col="weight",
+                robust=True,
+            )
 
     return cph
