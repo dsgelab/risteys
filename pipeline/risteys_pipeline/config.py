7490eef30cd043608500f84236491373fd24780d
essiviippola <essi.viippola@live.fi>
2022-02-24 13:29:11 +0200

    Update data path to minimal phenotype

--- pipeline/risteys_pipeline/config.py
+++ pipeline/risteys_pipeline/config.py
@@ -5,7 +5,7 @@ from pathlib import Path
 ROOT_DIR = Path("/data") / "processed_data"
 
 FINREGISTRY_MINIMAL_PHENOTYPE_DATA_PATH = (
-    ROOT_DIR / "minimal_phenotype" / "minimal_phenotype.feather"
+    ROOT_DIR / "minimal_phenotype" / "minimal_phenotype_2022-02-17.feather"
 )
 
 FINREGISTRY_LONG_FIRST_EVENTS_DATA_PATH = (
