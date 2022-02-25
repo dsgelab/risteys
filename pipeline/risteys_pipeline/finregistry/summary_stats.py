finregistry-pipeline: Fix prevalence computation
Previously prevalence was negative for all males because of sum(~minimal_phenotype["female"])

pipeline/risteys_pipeline/finregistry/summary_stats.py
@@ -29,44 +29,44 @@ def compute_key_figures(first_events, minimal_phenotype):
         minimal_phenotype(DataFrame): minimal phenotype dataframe
 
     Returns:
-        key_figures (DataFrame): key figures dataframe with the following columns:
+        kf (DataFrame): key figures dataframe with the following columns:
         endpoint, 
         nindivs_female, nindivs_male, nindivs_all, 
         mean_age_female, mean_age_male, mean_age_all,
         prevalence_female, prevalence_male, prevalence_all
     """
+    logger.info("Computing key figures")
+
     # Calculate the total number of individuals
     # Note: individuals for sex="unknown" is based on first events
     n_total = {
-        "female": sum(minimal_phenotype["female"]),
-        "male": sum(~minimal_phenotype["female"]),
-        "unknown": sum(first_events["sex"] == "unknown"),
+        "female": sum(minimal_phenotype["female"] == True),
+        "male": sum(minimal_phenotype["female"] == False),
+        "unknown": len(
+            first_events.loc[first_events["sex"] == "unknown", "finregistryid"].unique()
+        ),
     }
 
     # Calculate key figures by endpoint and sex
-    key_figures = (
+    kf = (
         first_events.groupby(["endpoint", "sex"])
         .agg({"finregistryid": "count", "age": "mean"})
         .rename(columns={"finregistryid": "nindivs_", "age": "mean_age_"})
         .fillna({"nindivs_": 0})
         .reset_index()
     )
-    key_figures["n_total"] = key_figures["sex"].replace(n_total)
-    key_figures["prevalence_"] = key_figures["nindivs_"] / key_figures["n_total"]
-    key_figures["weight"] = key_figures["n_total"] / sum(n_total.values())
+    kf["prevalence_"] = kf["nindivs_"] / kf["sex"].replace(n_total)
+    kf["n_endpoint"] = kf.groupby("endpoint")["nindivs_"].transform("sum")
+    kf["w"] = kf["nindivs_"] / kf["n_endpoint"]
 
     # Calculate key figures by endpoint for all individuals
-    key_figures_all = (
-        key_figures.groupby("endpoint")
+    kf_all = (
+        kf.groupby("endpoint")
         .agg(
             {
                 "nindivs_": "sum",
-                "mean_age_": lambda x: np.average(
-                    x, weights=key_figures.loc[x.index, "weight"]
-                ),
-                "prevalence_": lambda x: np.average(
-                    x, weights=key_figures.loc[x.index, "weight"]
-                ),
+                "mean_age_": lambda x: np.average(x, weights=kf.loc[x.index, "w"]),
+                "prevalence_": lambda x: np.average(x, weights=kf.loc[x.index, "w"]),
             }
         )
         .reset_index()
@@ -74,25 +74,23 @@ def compute_key_figures(first_events, minimal_phenotype):
     )
 
     # Drop rows with sex=unknown
-    key_figures = key_figures.loc[key_figures["sex"] != "unknown"]
-    key_figures = key_figures.reset_index(drop=True)
+    kf = kf.loc[kf["sex"] != "unknown"].reset_index(drop=True)
 
     # Combine the two datasets
-    key_figures = pd.concat([key_figures, key_figures_all])
+    kf = pd.concat([kf, kf_all])
 
     # Drop redundant columns
-    key_figures = key_figures.drop(columns=["weight", "n_total"])
+    kf = kf.drop(columns=["w", "n_endpoint"])
 
     # Remove personal data
-    indx = key_figures["nindivs_"] <= MIN_SUBJECTS_PERSONAL_DATA
     cols = ["nindivs_", "mean_age_", "prevalence_"]
-    key_figures.loc[indx, cols] = np.nan
+    kf.loc[kf["nindivs_"] <= MIN_SUBJECTS_PERSONAL_DATA, cols,] = np.nan
 
     # Pivot and flatten hierarchical columns
-    key_figures = key_figures.pivot(index="endpoint", columns="sex").reset_index()
-    key_figures.columns = ["".join(col).strip() for col in key_figures.columns.values]
+    kf = kf.pivot(index="endpoint", columns="sex").reset_index()
+    kf.columns = ["".join(col).strip() for col in kf.columns.values]
 
-    return key_figures
+    return kf
 
 
 def cumulative_incidence(cohort, all_cases, endpoints):