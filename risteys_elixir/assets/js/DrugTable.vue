91945e5d4e87355b09df36573804789a08fb31fb
SaraKuitunen <68216457+SaraKuitunen@users.noreply.github.com>
2022-03-08 14:23:12 +0200

    Fix table styling in surv analysis and drug tables (#112)
    Fix table styling in surv analysis and drug tables
    
    Allow HR results to wrap to make columns be inline
    
    Change column widths to make p-values fit better
    
    Remove redundant parts to have cleaner code
    
    Closes #112

--- risteys_elixir/assets/js/DrugTable.vue
+++ risteys_elixir/assets/js/DrugTable.vue
@@ -1,63 +1,61 @@
 <template>
-	<div class="scrolling">
-		<table class="w-full">
-			<thead>
-				<tr>
-					<th>
-						<div class="h-full border-b border-t pl-2 pt-4">  <!-- div hack to have the borders stay in place on scroll -->
-							Name <br>
-							<input
-								type="text"
-								placeholder="filter by drug name or ATC"
-								v-model="drug_filter"
-								v-on:keyup.stop="refresh_table()"
-								>
-						</div>
-					</th>
-					<th>
-						<div class="h-full border-t border-b">
-							<div><HelpDrugScore v-bind:phenocode="this.phenocode" /> Score</div>
-					<input type="radio" id="score_asc" value="score_asc" v-model="sorter" v-on:change="refresh_table()" checked><label for="score_asc" class="radio-left">▲</label><input type="radio" id="score_desc" value="score_desc" v-model="sorter" v-on:change="refresh_table()"><label for="score_desc" class="radio-right">▼</label>
-						</div>
-					</th>
-					<th>
-						<div class="h-full border-t border-b">
-							[95% CI]
-						</div>
-					</th>
-					<th>
-						<div class="h-full border-t border-b">
-							p <br>
-				<input type="radio" id="pvalue_asc" value="pvalue_asc" v-model="sorter" v-on:change="refresh_table()"><label for="pvalue_asc" class="radio-left">▲</label><input type="radio" id="pvalue_desc" value="pvalue_desc" v-model="sorter" v-on:change="refresh_table()"><label for="pvalue_desc" class="radio-right">▼</label>
-						</div>
-					</th>
-					<th>
-						<div class="h-full border-t border-b">
-							N <br>
-				<input type="radio" id="nindivs_asc" value="nindivs_asc" v-model="sorter" v-on:change="refresh_table()"><label for="nindivs_asc" class="radio-left">▲</label><input type="radio" id="nindivs_desc" value="nindivs_desc" v-model="sorter" v-on:change="refresh_table()"><label for="nindivs_desc" class="radio-right">▼</label>
-						</div>
-					</th>
-					<th>
-						<div class="h-full border-t border-b">
-							ATC code <br>
-				<input type="radio" id="atc_asc" value="atc_asc" v-model="sorter" v-on:change="refresh_table()"><label for="atc_asc" class="radio-left">▲</label><input type="radio" id="atc_desc" value="atc_desc" v-model="sorter" v-on:change="refresh_table()"><label for="atc_desc" class="radio-right">▼</label>
-						</div>
-					</th>
-				</tr>
-			</thead>
-			<tbody>
-				<template v-for="(drug, idx) in drug_table">
-					<tr v-bind:class="bg_class(idx)">
-						<th v-bind:class="bg_class(idx)">{{ drug.name }}</th>
-						<td>{{ drug.score_str }}</td>
-						<td>[{{ drug.ci_min_str }}, {{ drug.ci_max_str }}]</td>
-						<td>{{ drug.pvalue_str }}</td>
-						<td>{{ drug.n_indivs }}</td>
-						<td><a v-bind:href="drug.atc_link" target="_blank" rel="noopener noreferrer external">{{ drug.atc }}</a></td>
-					</tr>
-				</template>
-			</tbody>
-		</table>
+	<div role="table" class="drug-scrolling">
+		<div role="rowgroup">
+			<div role="row" class="grid-drug-header">
+				<div role="columnheader">
+					<p>Name</p>
+					<p class="end-align">
+						<input
+							type="text"
+							class="filter-input"
+							placeholder="filter by drug name or ATC"
+							v-model="drug_filter"
+							v-on:keyup.stop="refresh_table()"
+						>
+					</p>
+				</div>
+				<div role="columnheader">
+					<p><HelpDrugScore v-bind:phenocode="this.phenocode" /> Score</p>
+					<p class="end-align">
+						<input type="radio" id="score_asc" value="score_asc" v-model="sorter" v-on:change="refresh_table()" checked><label for="score_asc" class="radio-left">▲</label><input type="radio" id="score_desc" value="score_desc" v-model="sorter" v-on:change="refresh_table()"><label for="score_desc" class="radio-right">▼</label>
+					</p>
+				</div>
+				<div role="columnheader">
+					<p>[95% CI]</p>
+				</div>
+				<div role="columnheader">
+					<p>p </p>
+					<p class="end-align">
+						<input type="radio" id="pvalue_asc" value="pvalue_asc" v-model="sorter" v-on:change="refresh_table()"><label for="pvalue_asc" class="radio-left">▲</label><input type="radio" id="pvalue_desc" value="pvalue_desc" v-model="sorter" v-on:change="refresh_table()"><label for="pvalue_desc" class="radio-right">▼</label>
+					</p>
+				</div>
+				<div role="columnheader">
+					<p>N <p>
+					<p class="end-align">
+						<input type="radio" id="nindivs_asc" value="nindivs_asc" v-model="sorter" v-on:change="refresh_table()"><label for="nindivs_asc" class="radio-left">▲</label><input type="radio" id="nindivs_desc" value="nindivs_desc" v-model="sorter" v-on:change="refresh_table()"><label for="nindivs_desc" class="radio-right">▼</label>
+					</p>
+				</div>
+				<div role="columnheader">
+					<p>ATC code </p>
+					<p class="end-align">
+						<input type="radio" id="atc_asc" value="atc_asc" v-model="sorter" v-on:change="refresh_table()"><label for="atc_asc" class="radio-left">▲</label><input type="radio" id="atc_desc" value="atc_desc" v-model="sorter" v-on:change="refresh_table()"><label for="atc_desc" class="radio-right">▼</label>
+					</p>
+				</div>
+			</div>
+		</div>
+		<div role="rowgroup">
+			<template v-for="(drug, idx) in drug_table">
+				<div role="row" v-bind:class="bg_class(idx) + ' grid-drug-body'">
+					<div role="cell" v-bind:class="bg_class(idx)">{{ drug.name }}</div>
+					<div role="cell">{{ drug.score_str }}</div>
+					<div role="cell">[{{ drug.ci_min_str }}, {{ drug.ci_max_str }}]</div>
+					<div role="cell">{{ drug.pvalue_str }}</div>
+					<div role="cell">{{ drug.n_indivs }}</div>
+					<div role="cell"><a v-bind:href="drug.atc_link" target="_blank" rel="noopener noreferrer external">{{ drug.atc }}</a></div>
+				</div>
+			</template>
+		</div>
+
 		<template v-if="drug_table.length == 0">
 			<p>No data.</p>
 		</template>
@@ -159,65 +157,59 @@ export default {
 
 
 <style scoped>
-div.scrolling {
+.drug-scrolling {
 	max-width: 100%;
 	max-height: 500px;
 	overflow: scroll;
 	position: relative;
 
-	font-size: 0.8rem;
-}
-
-.scrolling table {
-	position: relative;
-	border-collapse: collapse;
-}
-
-.scrolling th, .scrolling td {
-	@apply px-1;
-}
-
-/* Allow div hack that shows borders on scroll */
-.scrolling thead th, .scrolling thead td {
+	/* If not set, scrolled text will appear above the table header.
+    We could have used padding-top: 0; only, but it actually looks better
+	with no padding at all.*/
 	padding: 0;
 }
 
-/* thead gray background */
-.scrolling thead th {
-	@apply bg-grey-lightest;
+ /* Reset default display: table; from browsers */
+[role="table"] {
+	display: block;
 }
 
-/* thead: drug cell */
-.scrolling thead tr:first-child th:first-child {
-	left: 0;
-	z-index: 1;
-	height: 80px;
-}
-
-/* thead: row */
-.scrolling thead tr th {
+[role="rowgroup"]:nth-child(1) {
 	position: sticky;
-
 	top: 0;
-	line-height: 1.5;
-	height: 80px;
+	background-color: #fafafa;
+	border-top-width: 1px;
+	border-bottom-width: 1px;
+
+	font-weight: bold;
 }
 
-/* tbody: leftmost column */
-.scrolling tbody th {
-  position: -webkit-sticky; /* for Safari */
-  position: sticky;
-  left: 0;
-  font-weight: normal;
+/* Grid table layout */
+.grid-drug-header {
+	display: grid;
+	grid-template-columns: 9fr 2fr 2fr 2fr 2fr 2fr;  /* first column spans 2 body columns */
+}
+.grid-drug-body {
+	display: grid;
+	grid-template-columns: 9fr 2fr 2fr 2fr 2fr 2fr;
 }
 
-@media (min-width: 650px) {
-	div.scrolling {
-		font-size: 1rem;
-	}
+/* Place table header widget near the bottom */
+[role="columnheader"] {
+	display: grid;
+	grid-template-columns: 1fr;
+}
+.end-align {
+	align-self: end;
+	margin-top: 0.25rem;
+}
 
-	.scrolling th, .scrolling td {
-		@apply px-3;
-	}
+/* Hide overflowing endpoint code name */
+[role="cell"]:nth-child(1) {
+	overflow: hidden;
+}
+
+.filter-input {
+	width: 40%;
 }
 </style>
