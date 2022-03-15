63d54e7b54c73611d3b28ed6f4446367c503b3e5
essiviippola <essi.viippola@live.fi>
2022-03-15 14:15:12 +0200

    finregistry-pipeline: Add age and year distributions
    Compute age and year distributions and write them to a file
    
    The distributions are not computed correctly in some cases which is captured by the test test_green_distribution_early_onset_trailing_individual_level_data in test_finregistry_summary_stats.py. The bug is described here: https://github.com/dsgelab/risteys/issues/122

--- pipeline/scripts/pipeline.py
+++ pipeline/scripts/pipeline.py
@@ -5,7 +5,7 @@ from risteys_pipeline.finregistry.load_data import (
     load_minimal_phenotype_data,
     load_first_events_data,
 )
-from risteys_pipeline.finregistry.summary_stats import compute_key_figures, cumulative_incidence
+from risteys_pipeline.finregistry.summary_stats import compute_distribution, compute_key_figures, cumulative_incidence
 from risteys_pipeline.finregistry.sample import get_cohort
 from risteys_pipeline.finregistry.survival_analysis import prep_all_cases
 
@@ -30,6 +30,12 @@ def pipeline():
     kf_all.to_csv(output_path + "/key_figures_all.csv", index=False)
     kf_index_persons = compute_key_figures(first_events, minimal_phenotype, index_persons=True)
     kf_index_persons.to_csv(output_path + "/key_figures_index_persons.csv", index=False)
+
+    # Run age and year distributions and write to a file
+    dist_age = compute_distribution(first_events, "age")
+    dist_age.to_csv(output_path + "/distribution_age.csv", index=True)
+    dist_year = compute_distribution(first_events, "year")
+    dist_year.to_csv(output_path + "/distribution_year.csv", index=True)
 
     # Run cumulative incidence and write to a file
     ci = cumulative_incidence(cohort, all_cases, endpoints)
