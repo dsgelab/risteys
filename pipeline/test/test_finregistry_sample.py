5a28c177281d32f467e6b1797c3bed268d1e55de
essiviippola <essi.viippola@live.fi>
2022-04-13 12:08:16 +0300

    finregistry-data: Optimize case-cohort weight computation
    To get the number of non-cases, we can compute `len(cohort) - len(cases)` instead of `len(set(cohort) - set(cases))` because `len(cohort) = len(cases) + len(non-cases)`
    
    Computation time dropped from ~4 seconds to nearly instant.

--- pipeline/test/test_finregistry_sample.py
+++ pipeline/test/test_finregistry_sample.py
@@ -1,13 +1,14 @@
 import numpy as np
+import pandas as pd
 from risteys_pipeline.finregistry.sample import calculate_case_cohort_weights
 
 
 def test_calculate_case_cohort_weights_cases():
     """Test case-cohort weight for cases"""
-    cases = [1, 2, 3, 4]
-    controls = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    sample_of_cases = [1, 2]
-    sample_of_controls = [2, 5, 7, 8]
+    cases = pd.Index([1, 2, 3, 4])
+    controls = pd.Index([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    sample_of_cases = pd.Index([1, 2])
+    sample_of_controls = pd.Index([2, 5, 7, 8])
     weight_cases, _ = calculate_case_cohort_weights(
         cases, controls, sample_of_cases, sample_of_controls
     )
@@ -17,10 +18,10 @@ def test_calculate_case_cohort_weights_cases():
 
 def test_calculate_case_cohort_weights_controls():
     """Test case-cohort weight for controls"""
-    cases = [1, 2, 3, 4]
-    controls = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    sample_of_cases = [1, 2]
-    sample_of_controls = [2, 5, 7, 8]
+    cases = pd.Index([1, 2, 3, 4])
+    controls = pd.Index([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    sample_of_cases = pd.Index([1, 2])
+    sample_of_controls = pd.Index([2, 5, 7, 8])
     _, weight_controls = calculate_case_cohort_weights(
         cases, controls, sample_of_cases, sample_of_controls
     )
@@ -30,10 +31,10 @@ def test_calculate_case_cohort_weights_controls():
 
 def test_calculate_case_cohort_weights_no_noncases():
     """Test case-cohort weights if there is no non-cases"""
-    cases = [1, 2, 3, 4, 5]
-    controls = [1, 2, 3, 4, 5]
-    sample_of_cases = [1, 2]
-    sample_of_controls = [2, 3, 4, 5]
+    cases = pd.Index([1, 2, 3, 4, 5])
+    controls = pd.Index([1, 2, 3, 4, 5])
+    sample_of_cases = pd.Index([1, 2])
+    sample_of_controls = pd.Index([2, 3, 4, 5])
     weight_cases, _ = calculate_case_cohort_weights(
         cases, controls, sample_of_cases, sample_of_controls
     )
