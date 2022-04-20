690d0b4dded35a63cfa730d3fb5ab8d76120fc39
Vincent <vincent-octo@users.noreply.github.com>
2022-04-20 12:03:18 +0300

    pipe: use new pipeline for FinnGen
    This commit ports some parts the FinnGen pipeline to the new pipeline
    developed by Essi Viippola. This new pipeline was initially made for
    FinRegistry.
    
    Currently ported:
    - key figures
    - age and year at first-event distributions
    - cumulative incidence
    
    The FinnGen pipeline now shares code with the FinRegistry pipeline,
    however there are some differences:
    - the FinnGen pipeline doesn't use "index persons"
    - Key Figures and distributions are computed for all endpoints in the
      FinnGen pipeline, not just core endpoints.
    
    GitHub: closes #129

--- pipeline/risteys_pipeline/utils/utils.py
+++ pipeline/risteys_pipeline/utils/utils.py
@@ -1,6 +1,10 @@
 """Utils functions"""
+from contextlib import contextmanager
 
 import pandas as pd
+
+from risteys_pipeline.log import logger
+
 
 DAYS_IN_YEAR = 365.25
 
@@ -11,3 +15,14 @@ def to_decimal_year(date_col):
     decimal_year = date_col.dt.year + (date_col.dt.dayofyear - 1) / DAYS_IN_YEAR
     return decimal_year
 
+
+@contextmanager
+def log_if_diff(name, count_func):
+    """Warn if the context body changes the result of `count_func`"""
+    count_pre = count_func()
+    yield
+    count_post = count_func()
+
+    diff = count_post - count_pre
+    if diff != 0:
+        logger.warning(f"{name} Δ = {diff} ({100 * (count_post - count_pre) / count_pre:.2f}%)")
