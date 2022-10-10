7d36bef88349d30fb8c1e84d5243c56737e4fb77
SaraKuitunen <sarajohannakuitunen@gmail.com>
2022-10-10 14:24:44 +0300

    Allow having intermediate counts also from FR
    resolves #197

--- risteys_elixir/lib/risteys/fg_endpoint/explainer_step.ex
+++ risteys_elixir/lib/risteys/fg_endpoint/explainer_step.ex
@@ -8,6 +8,7 @@ defmodule Risteys.FGEndpoint.ExplainerStep do
     field :step, :string
     # "nil" will indicate individual-level data: 1, 2, 3, or 4.
     field :nindivs, :integer
+    field :dataset, :string
 
     timestamps()
   end
@@ -15,13 +16,14 @@ defmodule Risteys.FGEndpoint.ExplainerStep do
   @doc false
   def changeset(explainer_step, attrs) do
     explainer_step
-    |> cast(attrs, [:fg_endpoint_id, :step, :nindivs])
+    |> cast(attrs, [:fg_endpoint_id, :step, :nindivs, :dataset])
     # :nindivs can be "nil"
-    |> validate_required([:fg_endpoint_id, :step])
+    |> validate_required([:fg_endpoint_id, :step, :dataset])
 
     # Making sure no individual-level data comes in
     |> validate_number(:nindivs, greater_than_or_equal_to: 0)
     |> validate_exclusion(:nindivs, 1..4)
-    |> unique_constraint(:fg_endpoint_id, name: :fg_endpoint_step)
+    |> validate_inclusion(:dataset, ["FG", "FR"])
+    |> unique_constraint(:fg_endpoint_id, name: :fg_endpoint_step_dataset)
   end
 end
