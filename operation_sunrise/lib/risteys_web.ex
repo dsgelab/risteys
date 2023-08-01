ff2d4dfe84762e5c65ae3e5aa1b575b3ca5b582c
Vincent <vincent-octo@users.noreply.github.com>
2023-08-01 13:17:54 +0300

    web: moving CSS files to static paths

--- operation_sunrise/lib/risteys_web.ex
+++ operation_sunrise/lib/risteys_web.ex
@@ -17,7 +17,7 @@ defmodule RisteysWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets css fonts images favicon.ico robots.txt)
 
   def router do
     quote do
