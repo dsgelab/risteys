01c39f4afc817a6909f8070b9f57ef38141c2252
Vincent <vincent-octo@users.noreply.github.com>
2023-08-10 14:33:32 +0300

    web: add relationships table

--- operation_sunrise/assets/js/app.js
+++ operation_sunrise/assets/js/app.js
@@ -17,21 +17,43 @@
 
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
-// Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
 
-// connect if there are any LiveViews on the page
-liveSocket.connect()
+/*
+ * Hooks: Phoenix <--> JS
+ */
+let Hooks = {}
 
-// expose liveSocket on window for web console debug logs and latency simulation:
-// >> liveSocket.enableDebug()
-// >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
-// >> liveSocket.disableLatencySim()
-window.liveSocket = liveSocket
+/*
+ * Risteys
+ */
+import { drawCompBox } from './CompBox.js';
+
+/* Draw CompBoxes
+ *
+ * For them to allways appear we would need to:
+ * 1. draw when the static HTML is rendered by Phoenix
+ * 2. draw on LiveView 'mounted'
+ * 3. draw on LiveView 'updated'
+ *
+ * However, we don't do (1.) here because the initial static render is intentionally rendering
+ * 0 elements to prevent blocking the initial page render (it takes ~ 5s to render).
+ * So instead we just do (2.) and (3.)
+ */
+function replaceWithCompBox(element) {
+    // The element MUST have a 'data-compbox-value'
+    const value = element.dataset.compboxValue
+    element.innerHTML = drawCompBox(value)
+}
+
+Hooks.DrawCompBox = {
+    mounted() {
+        replaceWithCompBox(this.el)
+    },
+    updated() {
+        replaceWithCompBox(this.el)
+    }
+}
 
 /*
  * UTILS
@@ -46,3 +68,23 @@ function init_search_key () {
 }
 
 init_search_key()
+
+
+/***********
+ * Phoenix *
+ **********/
+// Establish Phoenix Socket and LiveView configuration.
+import {Socket} from "phoenix"
+import {LiveSocket} from "phoenix_live_view"
+
+let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
+let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}, hooks: Hooks})
+
+// connect if there are any LiveViews on the page
+liveSocket.connect()
+
+// expose liveSocket on window for web console debug logs and latency simulation:
+// >> liveSocket.enableDebug()
+// >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
+// >> liveSocket.disableLatencySim()
+window.liveSocket = liveSocket
